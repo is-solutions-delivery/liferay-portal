b6c9503242cf3ba40d9d2e0db00faef99a4b3fe8
Aildson Ferreira <aildson.ferreira@liferay.com>
2021-12-13 14:03:28 -0300

    LPS-140319 SF

--- modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/routes/customer-portal/components/SubscriptionsNavbar/index.js
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/routes/customer-portal/components/SubscriptionsNavbar/index.js
@@ -3,7 +3,12 @@ import React, {useEffect, useState} from 'react';
 import {getAccountSubscriptionGroupsByFilter} from '../../../../common/services/liferay/graphql/queries';
 import SubscriptionsFilterByStatus from '../SubscriptionsFilterByStatus';
 
-const SubscriptionsNavbar = ({accountKey, selectedStatus, setSelectedStatus, setSelectedSubscriptionGroup}) => {
+const SubscriptionsNavbar = ({
+	accountKey,
+	selectedStatus,
+	setSelectedStatus,
+	setSelectedSubscriptionGroup,
+}) => {
 	const [subscriptionsTags, setSubscriptionsTags] = useState(() => []);
 
 	const {
