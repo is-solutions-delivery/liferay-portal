ad53be1ccd662467323fe6a51ad1ab5814da301d
Nícolas Moura <nicolas_moura02@outlook.com>
2022-01-28 16:42:22 -0300

    LPS-145568 Rename and SF

--- modules/apps/site-initializer/site-initializer-extender/site-initializer-extender/src/main/java/com/liferay/site/initializer/extender/internal/BundleSiteInitializer.java
+++ modules/apps/site-initializer/site-initializer-extender/site-initializer-extender/src/main/java/com/liferay/site/initializer/extender/internal/BundleSiteInitializer.java
@@ -2970,7 +2970,7 @@ public class BundleSiteInitializer implements SiteInitializer {
 		CPInstance cpInstance =
 			_commerceReferencesHolder.cpInstanceLocalService.getCPInstance(
 				cpDefinition.getCPDefinitionId(),
-				cpInstancePropertiesJSONObject.getString("cpInstanceSKu"));
+				cpInstancePropertiesJSONObject.getString("cpInstanceSku"));
 
 		if (cpInstance == null) {
 			return;
