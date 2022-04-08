8704540d2dee7587271e9c979217b5537dd52a7f
Mylena Monte <mylena.monte@liferay.com>
2022-04-08 11:15:27 -0300

    LPS-151187  Adjust login page information to be the same as the dashboard

--- modules/apps/site-initializer/site-initializer-raylife-ap/src/main/resources/site-initializer/fragments/src/agent-portal/fragments/sign-in/index.js
+++ modules/apps/site-initializer/site-initializer-raylife-ap/src/main/resources/site-initializer/fragments/src/agent-portal/fragments/sign-in/index.js
@@ -16,20 +16,17 @@ const EMAIL_INPUT_ID = '_com_liferay_login_web_portlet_LoginPortlet_login';
 const PASSWORD_INPUT_ID =
 	'_com_liferay_login_web_portlet_LoginPortlet_password';
 
-const emailInput = document.getElementById(EMAIL_INPUT_ID);
-emailInput.setAttribute('placeholder', 'Email');
-emailInput.removeAttribute('value');
-
-const passwordInput = document.getElementById(PASSWORD_INPUT_ID);
-passwordInput.setAttribute('placeholder', 'Password');
-
-window.onload = function () {
-	document.getElementById(EMAIL_INPUT_ID).focus();
-};
-
-document.getElementById(EMAIL_INPUT_ID).focus();
-
 function inputValidation() {
+	const emailInput = document.getElementById(EMAIL_INPUT_ID);
+	const passwordInput = document.getElementById(PASSWORD_INPUT_ID);
+
+	window.onload = function () {
+		emailInput.focus();
+	};
+
+	emailInput.focus();
+
+
 	const EMAIL_REGEX = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g;
 
 	const TEXT_VARIANT = {
