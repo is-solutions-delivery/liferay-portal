97127a3ad10f3b1ad9dcbfef63a15e6b262a40dd
Keven Leone <keven.leone@liferay.com>
2022-05-03 14:34:35 -0300

    LPS-152664 Subscribe to Question when add a reply

--- modules/apps/questions/questions-web/src/main/resources/META-INF/resources/js/components/Answer.es.js
+++ modules/apps/questions/questions-web/src/main/resources/META-INF/resources/js/components/Answer.es.js
@@ -40,6 +40,7 @@ export default withRouter(
 		deleteAnswer,
 		editable = true,
 		match: {url},
+		onSubscription,
 	}) => {
 		const [comments, setComments] = useState(
 			answer.messageBoardMessages.items
@@ -272,6 +273,7 @@ export default withRouter(
 							commentsChange={_commentsChange}
 							editable={editable}
 							entityId={answer.id}
+							onSubscription={onSubscription}
 							showNewComment={showNewComment}
 							showNewCommentChange={(value) =>
 								setShowNewComment(value)
