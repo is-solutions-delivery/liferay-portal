2153352b1f9a0a29f68c4fa317da7ac8aacaef4d
Felipe Veloso <felipe.veloso@liferay.com>
2022-07-26 10:41:32 -0300

    LPS-152322 formatSource

--- modules/apps/headless/headless-delivery/headless-delivery-impl/src/main/java/com/liferay/headless/delivery/internal/dto/v1_0/converter/MessageBoardSuspiciousActivityDTOConverter.java
+++ modules/apps/headless/headless-delivery/headless-delivery-impl/src/main/java/com/liferay/headless/delivery/internal/dto/v1_0/converter/MessageBoardSuspiciousActivityDTOConverter.java
@@ -1,59 +1,72 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
 package com.liferay.headless.delivery.internal.dto.v1_0.converter;
 
-import com.liferay.headless.delivery.dto.v1_0.MessageBoardMessage;
 import com.liferay.headless.delivery.dto.v1_0.MessageBoardSuspiciousActivity;
-import com.liferay.message.boards.model.MBMessage;
 import com.liferay.message.boards.model.MBSuspiciousActivity;
 import com.liferay.message.boards.service.MBSuspiciousActivityService;
 import com.liferay.portal.vulcan.dto.converter.DTOConverter;
 import com.liferay.portal.vulcan.dto.converter.DTOConverterContext;
+
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
+/**
+ * @author Felipe Veloso
+ */
 @Component(
 	property = "dto.class.name=com.liferay.message.boards.model.MBSuspiciousActivity",
-	service = {DTOConverter.class, MessageBoardSuspiciousActivityDTOConverter.class}
+	service = {
+		DTOConverter.class, MessageBoardSuspiciousActivityDTOConverter.class
+	}
 )
-
-public class MessageBoardSuspiciousActivityDTOConverter implements
-	DTOConverter<MBSuspiciousActivity, MessageBoardSuspiciousActivity> {
+public class MessageBoardSuspiciousActivityDTOConverter
+	implements DTOConverter
+		<MBSuspiciousActivity, MessageBoardSuspiciousActivity> {
 
 	@Override
-	public MessageBoardSuspiciousActivity toDTO(DTOConverterContext dtoConverterContext)
+	public String getContentType() {
+		return MessageBoardSuspiciousActivity.class.getSimpleName();
+	}
+
+	@Override
+	public MessageBoardSuspiciousActivity toDTO(
+			DTOConverterContext dtoConverterContext)
 		throws Exception {
 
-		MBSuspiciousActivity mbSuspiciousActivity = _mbSuspiciousActivityService.getSuspiciousActivity(
-			(Long)dtoConverterContext.getId());
+		MBSuspiciousActivity mbSuspiciousActivity =
+			_mbSuspiciousActivityService.getSuspiciousActivity(
+				(Long)dtoConverterContext.getId());
 
-
-		return new MessageBoardSuspiciousActivity(){
+		return new MessageBoardSuspiciousActivity() {
 			{
-
 				createDate = mbSuspiciousActivity.getCreateDate();
 				description = mbSuspiciousActivity.getDescription();
 				messageId = mbSuspiciousActivity.getMessageId();
 				modifiedDate = mbSuspiciousActivity.getModifiedDate();
-				suspiciousActivityId = mbSuspiciousActivity.getSuspiciousActivityId();
+				suspiciousActivityId =
+					mbSuspiciousActivity.getSuspiciousActivityId();
 				threadId = mbSuspiciousActivity.getThreadId();
 				type = mbSuspiciousActivity.getType();
 				userId = mbSuspiciousActivity.getUserId();
 				validated = mbSuspiciousActivity.getValidated();
-
-
-
-
 			}
-
 		};
-
-	}
-
-	@Override
-	public String getContentType() {
-
-		return MessageBoardSuspiciousActivity.class.getSimpleName();
 	}
 
 	@Reference
 	private MBSuspiciousActivityService _mbSuspiciousActivityService;
+
 }
