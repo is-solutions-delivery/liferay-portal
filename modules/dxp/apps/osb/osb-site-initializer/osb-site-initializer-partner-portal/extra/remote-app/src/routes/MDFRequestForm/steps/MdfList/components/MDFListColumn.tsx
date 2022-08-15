50b46dc5f31b9b4fc9c5c982744a0504a3e0b306
Bruno Santana <bruno.santana@liferay.com>
2022-08-15 15:55:12 -0300

    LPS-159818  Changed the name of archive

---
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-partner-portal/extra/remote-app/src/routes/MDFRequestForm/steps/MdfList/components/MDFListColumn.tsx
@@ -0,0 +1,69 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ */
+
+import ClayIcon from '@clayui/icon';
+import DropDown from './Dropdown';
+
+const MDFListColumn = [
+	{label: 'Request ID', columnKey: 'requestId'},
+	{
+		label: 'Status',
+		columnKey: 'status',
+		render: (status: string) => (
+			<div className="align-items-center">
+				<ClayIcon className='mr-1' symbol="simple-circle" />
+				{status}
+			</div>
+		),
+	},
+	{label: 'Activity Period', columnKey: 'activityPeriod'},
+	{label: 'Total Cost', columnKey: 'totalCost'},
+	{label: 'Requested', columnKey: 'requested'},
+	{label: 'Approved', columnKey: 'approved'},
+
+	{
+		label: 'Reimpursement Claim(s)',
+		columnKey: 'reimpursementClaim',
+	},
+	{
+		label: '',
+		columnKey: '',
+		render: () => (
+			<div>
+				<ClayIcon symbol="comments" />
+			</div>
+		),
+	},
+	{
+		label: '',
+		columnKey: '',
+		render: () => (
+			<div>
+				<DropDown
+					optionList={[
+						{
+							icon: 'check',
+							label: 'Approve',
+							optionKey: 'approve',
+						},
+						{
+							icon: 'times',
+							label: 'Reject',
+							optionKey: 'reject',
+						},
+					]}
+				></DropDown>
+			</div>
+		),
+	},
+];
+
+export default MDFListColumn;
