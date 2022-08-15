b988724f8f859a5b63c75d885131546758a55ccb
Claudionilton <claudionilton.junior@liferay.com>
2022-08-15 14:11:30 -0300

    LPS-160514 Search 's Loading Time

--- modules/apps/questions/questions-web/src/main/resources/META-INF/resources/js/pages/questions/Questions.es.js
+++ modules/apps/questions/questions-web/src/main/resources/META-INF/resources/js/pages/questions/Questions.es.js
@@ -282,8 +282,7 @@ export default withRouter(
 					filter += `${
 						(section && section.id && ' and ') || ''
 					}keywords/any(x:x eq '${keywords}')`;
-				}
-				else if (creatorId) {
+				} else if (creatorId) {
 					const operand = filter ? 'and' : '';
 
 					filter += `${operand} creator/id eq ${creatorId}`;
@@ -334,23 +333,19 @@ export default withRouter(
 					siteKey,
 					'dateModified:desc'
 				);
-			}
-			else if (filter === 'week') {
+			} else if (filter === 'week') {
 				const date = new Date();
 				date.setDate(date.getDate() - 7);
 
 				fn = getRankedThreadsCallback(date, page, pageSize, section);
-			}
-			else if (filter === 'month') {
+			} else if (filter === 'month') {
 				const date = new Date();
 				date.setDate(date.getDate() - 31);
 
 				fn = getRankedThreadsCallback(date, page, pageSize, section);
-			}
-			else if (filter === 'most-voted') {
+			} else if (filter === 'most-voted') {
 				fn = getRankedThreadsCallback(null, page, pageSize, section);
-			}
-			else {
+			} else {
 				fn = getThreadsCallback(
 					creatorId,
 					currentTag,
@@ -403,8 +398,7 @@ export default withRouter(
 			}
 			if (search) {
 				url += `?search=${search}&`;
-			}
-			else {
+			} else {
 				url += '?';
 			}
 
@@ -419,7 +413,7 @@ export default withRouter(
 
 		const [debounceCallback] = useDebounceCallback(
 			(search) => changePage(search, 1, 20),
-			500
+			1000
 		);
 
 		useEffect(() => {
@@ -437,15 +431,13 @@ export default withRouter(
 						setSection(data.messageBoardSections.items[0]);
 						setSectionQuery(getSectionBySectionTitleQuery);
 						setSectionQueryVariables(variables);
-					}
-					else {
+					} else {
 						setSection(null);
 						setError({message: 'Loading Topics', title: 'Error'});
 						setLoading(false);
 					}
 				});
-			}
-			else if (sectionTitle === ALL_SECTIONS_ID) {
+			} else if (sectionTitle === ALL_SECTIONS_ID) {
 				const variables = {siteKey: context.siteKey};
 				getSections({
 					variables,
@@ -493,8 +485,7 @@ export default withRouter(
 							: '#'
 					}/questions/${sectionTitle}/new`
 				);
-			}
-			else {
+			} else {
 				historyPushParser(`/questions/${sectionTitle}/new`);
 			}
 
@@ -759,7 +750,7 @@ export default withRouter(
 												<ClayButtonWithIcon
 													displayType="unstyled"
 													onClick={() => {
-														debounceCallback('');
+														setSearch('');
 													}}
 													symbol="times-circle"
 													type="submit"
