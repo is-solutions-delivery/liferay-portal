a68561a83e545d041699f68fc5a6feba76104024
Elias Santos <elias.santos@liferay.com>
2022-08-29 12:31:05 -0300

    LPS-161728 Commerce Catalog API tweaks

--- modules/apps/site-initializer/site-initializer-raylife-d2c/extra/remote-app/src/common/services/liferay/api.js
+++ modules/apps/site-initializer/site-initializer-raylife-d2c/extra/remote-app/src/common/services/liferay/api.js
@@ -13,15 +13,11 @@
  */
 
 import _axios from 'axios';
-import {Liferay} from '../../utils/liferay';
 
 const {REACT_APP_LIFERAY_API = window.location.origin} = process.env;
 
 const axios = _axios.create({
 	baseURL: REACT_APP_LIFERAY_API,
-	headers: {
-		'x-csrf-token': Liferay.authToken,
-	},
 });
 
 export {axios};
