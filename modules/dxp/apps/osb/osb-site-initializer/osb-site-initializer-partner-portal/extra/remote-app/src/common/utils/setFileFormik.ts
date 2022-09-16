6a05f891eebc8649abf3fee93cae6423eea41dfd
Joao Silva <joao.silva@liferay.com>
2022-09-16 11:35:42 -0300

    LPS-162414 Create input file

---
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-partner-portal/extra/remote-app/src/common/utils/setFileFormik.ts
@@ -0,0 +1,55 @@
+/* The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ */
+
+const getFileReader = (file: any) => {
+	return new Promise<{result: unknown}>((resolve, reject) => {
+		const fileReader: any = new FileReader();
+
+		fileReader.readAsDataURL(file);
+
+		fileReader.onload = () => {
+			resolve(fileReader);
+		};
+
+		fileReader.onerror = reject;
+	});
+};
+
+export default async function setFileFormik(
+	file: any,
+	name: string,
+	idActivity?: number,
+	idBudget?: number,
+	setFieldValue?: (
+		field: string,
+		value: any,
+		shouldValidate?: boolean | undefined
+	) => void,
+	typeDocument?: string
+) {
+	const fileReader = await getFileReader(file[0]);
+
+	const currentFile = {fileURL: file, idActivities: 0, idBudget: 0, type: ''};
+	currentFile.fileURL = fileReader.result;
+
+	if (idActivity) {
+		currentFile.idActivities = idActivity;
+	}
+
+	if (idBudget) {
+		currentFile.idBudget = idBudget;
+	}
+
+	if (typeDocument) {
+		currentFile.type = typeDocument;
+	}
+
+	if (setFieldValue) {
+		setFieldValue(`${name}`, currentFile);
+	}
+}
