054cd350f140a87af66f2d92d25d02c5a7362e5c
Nilton Vieira <nilton.vieira@liferay.com>
2022-09-21 16:33:48 -0300

    LPS-160961 not needed

--- modules/apps/site-initializer/site-initializer-extender/site-initializer-extender/src/main/java/com/liferay/site/initializer/extender/internal/BundleSiteInitializer.java
+++ modules/apps/site-initializer/site-initializer-extender/site-initializer-extender/src/main/java/com/liferay/site/initializer/extender/internal/BundleSiteInitializer.java
@@ -3074,8 +3074,8 @@ public class BundleSiteInitializer implements SiteInitializer {
 	}
 
 	private void _addSegmentsExperiences(
-		ServiceContext serviceContext,
-		Map<String, String> segmentsEntriesIdsStringUtilReplaceValues)
+			ServiceContext serviceContext,
+			Map<String, String> segmentsEntriesIdsStringUtilReplaceValues)
 		throws Exception {
 
 		String json = SiteInitializerUtil.read(
@@ -3949,9 +3949,7 @@ public class BundleSiteInitializer implements SiteInitializer {
 	private Map<String, String> _getClassNameIdStringUtilReplaceValues() {
 		Map<String, String> map = new HashMap<>();
 
-		Class<?>[] classes = {
-			DDMStructure.class, JournalArticle.class, Layout.class
-		};
+		Class<?>[] classes = {DDMStructure.class, JournalArticle.class};
 
 		for (Class<?> clazz : classes) {
 			map.put(
