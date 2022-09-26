fc1b1b07811da74fd117296d2fa6f5aa8361de86
DiogoNo <diogo.nobrega@liferay.com>
2022-09-26 09:22:53 -0300

    LPS-162414 adjust Deploy

---
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-partner-portal/extra/remote-app/src/common/interfaces/mdfClaim.ts
@@ -0,0 +1,23 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ */
+
+import LiferayObject from './liferayObject';
+import MDFClaimActivity from './mdfClaimActivity';
+import MDFClaimDocuments from './mdfClaimDocuments';
+export default interface MDFClaim extends Partial<LiferayObject> {
+	mdfClaimActivities: MDFClaimActivity[];
+	mdfClaimDocuments: {
+		claims: MDFClaimDocuments[];
+		budgets: MDFClaimDocuments[];
+		activities: MDFClaimDocuments[];
+	};
+	totalClaimAmount: number;
+}
