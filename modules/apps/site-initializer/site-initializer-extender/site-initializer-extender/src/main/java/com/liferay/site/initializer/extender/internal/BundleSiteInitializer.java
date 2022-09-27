eafe7742783706a7b05f0092834368732ffbf3b3
Gleice Lisbino <gleice.lisbino@liferay.com>
2022-09-27 13:42:28 -0300

    LPS-164128 Rename _addOrganization

--- modules/apps/site-initializer/site-initializer-extender/site-initializer-extender/src/main/java/com/liferay/site/initializer/extender/internal/BundleSiteInitializer.java
+++ modules/apps/site-initializer/site-initializer-extender/site-initializer-extender/src/main/java/com/liferay/site/initializer/extender/internal/BundleSiteInitializer.java
@@ -2245,7 +2245,7 @@ public class BundleSiteInitializer implements SiteInitializer {
 		}
 	}
 
-	private void _addOrganization(
+	private void _addOrUpdateOrganization(
 			String json, Organization parentOrganization,
 			ServiceContext serviceContext)
 		throws Exception {
@@ -2305,7 +2305,7 @@ public class BundleSiteInitializer implements SiteInitializer {
 		}
 
 		for (int i = 0; i < jsonArray.length(); i++) {
-			_addOrganization(
+			_addOrUpdateOrganization(
 				jsonArray.getString(i), organization, serviceContext);
 		}
 	}
@@ -2328,7 +2328,7 @@ public class BundleSiteInitializer implements SiteInitializer {
 				return;
 			}
 
-			_addOrganization(json, null, serviceContext);
+			_addOrUpdateOrganization(json, null, serviceContext);
 		}
 	}
 
