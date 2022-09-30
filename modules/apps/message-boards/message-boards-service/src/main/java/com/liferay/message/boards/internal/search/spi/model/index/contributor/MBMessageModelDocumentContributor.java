58ebaf53155710e19ea973126dbf5c07b4924624
Felipe Veloso <felipe.veloso@liferay.com>
2022-09-30 08:14:07 -0300

    LPS-163838 auto SF

--- modules/apps/message-boards/message-boards-service/src/main/java/com/liferay/message/boards/internal/search/spi/model/index/contributor/MBMessageModelDocumentContributor.java
+++ modules/apps/message-boards/message-boards-service/src/main/java/com/liferay/message/boards/internal/search/spi/model/index/contributor/MBMessageModelDocumentContributor.java
@@ -219,7 +219,7 @@ public class MBMessageModelDocumentContributor
 
 	@Reference
 	private MBMessageLocalService _mbMessageLocalService;
-	
+
 	@Reference
 	private RatingsStatsLocalService _ratingsStatsLocalService;
 
