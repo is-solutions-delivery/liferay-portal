a5163e350f041a710b36e730213d2cd3206da885
Gleice Lisbino <gleice.lisbino@liferay.com>
2022-10-27 16:45:01 -0300

    LPS-166913 getSiteMessageBoardMessagesMyActivityPage

--- modules/apps/headless/headless-delivery/headless-delivery-impl/src/main/java/com/liferay/headless/delivery/internal/resource/v1_0/MessageBoardMessageResourceImpl.java
+++ modules/apps/headless/headless-delivery/headless-delivery-impl/src/main/java/com/liferay/headless/delivery/internal/resource/v1_0/MessageBoardMessageResourceImpl.java
@@ -339,6 +339,34 @@ public class MessageBoardMessageResourceImpl
 			Sort[] sorts)
 		throws Exception {
 
+		return _getMessageBoardMessagesPage(
+			HashMapBuilder.put(
+				"deleteBatch",
+				addAction(
+					ActionKeys.DELETE, "deleteMessageBoardMessageBatch",
+					MBConstants.RESOURCE_NAME, null)
+			).put(
+				"get",
+				addAction(
+					ActionKeys.VIEW, "getSiteMessageBoardMessagesPage",
+					MBConstants.RESOURCE_NAME, siteId)
+			).put(
+				"updateBatch",
+				addAction(
+					ActionKeys.UPDATE, "putMessageBoardMessageBatch",
+					MBConstants.RESOURCE_NAME, null)
+			).build(),
+			null, siteId, flatten, search, aggregation, filter, pagination,
+			sorts);
+	}
+
+	@Override
+	public Page<MessageBoardMessage> getSiteMessageBoardMessagesMyActivityPage(
+		Long siteId, Boolean flatten, String search,
+		Aggregation aggregation, Filter filter, Pagination pagination,
+		Sort[] sorts)
+		throws Exception {
+
 		Page<MessageBoardMessage> messageBoardMessagesPage =
 			_getMessageBoardMessagesPage(
 				HashMapBuilder.put(
@@ -366,7 +394,7 @@ public class MessageBoardMessageResourceImpl
 		List<MessageBoardMessage> messageBoardMessageList = new ArrayList<>();
 
 		for (MessageBoardMessage messageBoardMessage1 :
-				messageBoardMessageCollection) {
+			messageBoardMessageCollection) {
 
 			Comparator<MessageBoardMessage> comparator = Comparator.comparing(
 				MessageBoardMessage::getDateModified);
@@ -392,25 +420,7 @@ public class MessageBoardMessageResourceImpl
 			}
 		}
 
-		return _getMessageBoardMessagesPage(
-			HashMapBuilder.put(
-				"deleteBatch",
-				addAction(
-					ActionKeys.DELETE, "deleteMessageBoardMessageBatch",
-					MBConstants.RESOURCE_NAME, null)
-			).put(
-				"get",
-				addAction(
-					ActionKeys.VIEW, "getSiteMessageBoardMessagesPage",
-					MBConstants.RESOURCE_NAME, siteId)
-			).put(
-				"updateBatch",
-				addAction(
-					ActionKeys.UPDATE, "putMessageBoardMessageBatch",
-					MBConstants.RESOURCE_NAME, null)
-			).build(),
-			null, siteId, flatten, search, aggregation, filter, pagination,
-			sorts);
+		return Page.of(messageBoardMessageList);
 	}
 
 	@Override
