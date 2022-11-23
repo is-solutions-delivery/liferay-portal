641ca60a457248f6ddf58d11faa7b58d6c8d326e
Tamyris Torres <tamyris.bernardo@liferay.com>
2022-11-23 17:24:47 -0300

    LPS-167118 SF

--- modules/apps/site-initializer/site-initializer-extender/site-initializer-extender/src/main/java/com/liferay/site/initializer/extender/internal/BundleSiteInitializer.java
+++ modules/apps/site-initializer/site-initializer-extender/site-initializer-extender/src/main/java/com/liferay/site/initializer/extender/internal/BundleSiteInitializer.java
@@ -1182,7 +1182,7 @@ public class BundleSiteInitializer implements SiteInitializer {
 						objectDefinition);
 
 				if (!json.contains("enableComments") &
-				!json.contains("accountEntryRestrictedObjectFieldName")) {
+					!json.contains("accountEntryRestrictedObjectFieldName")) {
 
 					objectDefinitionResource.postObjectDefinitionPublish(
 						objectDefinition.getId());
