f14544c734dadc3858f15350ca7f64f2712f10e4
Nilton Vieira <nilton.vieira@liferay.com>
2022-11-23 16:00:42 -0300

    LPS-169377 get groupId instead of classPk

--- modules/apps/site-initializer/site-initializer-extender/site-initializer-extender/src/main/java/com/liferay/site/initializer/extender/internal/BundleSiteInitializer.java
+++ modules/apps/site-initializer/site-initializer-extender/site-initializer-extender/src/main/java/com/liferay/site/initializer/extender/internal/BundleSiteInitializer.java
@@ -3458,7 +3458,7 @@ public class BundleSiteInitializer implements SiteInitializer {
 			List<Long> groupIds = new ArrayList<>();
 
 			for (int j = 0; j < groupsJSONArray.length(); j++) {
-				JSONObject groupJSONObject = jsonArray.getJSONObject(i);
+				JSONObject groupJSONObject = groupsJSONArray.getJSONObject(j);
 
 				String groupType = groupJSONObject.getString("groupType");
 
@@ -3468,7 +3468,10 @@ public class BundleSiteInitializer implements SiteInitializer {
 							serviceContext.getCompanyId(),
 							groupJSONObject.getString("groupName"));
 
-					groupIds.add(organization.getOrganizationId());
+					Group group =_groupLocalService.getOrganizationGroup(
+						serviceContext.getCompanyId(), organization.getOrganizationId());
+
+					groupIds.add(group.getGroupId());
 				}
 				else if (StringUtil.equals(groupType, "Site")) {
 					groupIds.add(serviceContext.getScopeGroupId());
@@ -3478,14 +3481,21 @@ public class BundleSiteInitializer implements SiteInitializer {
 						serviceContext.getCompanyId(),
 						groupJSONObject.getString("groupName"));
 
-					groupIds.add(user.getUserId());
+					Group group = _groupLocalService.getUserGroup(
+						serviceContext.getCompanyId(), user.getUserId());
+
+					groupIds.add(group.getGroupId());
 				}
 				else if (StringUtil.equals(groupType, "UserGroups")) {
 					UserGroup userGroup = _userGroupLocalService.fetchUserGroup(
 						serviceContext.getCompanyId(),
 						groupJSONObject.getString("groupName"));
 
-					groupIds.add(userGroup.getUserGroupId());
+					Group group = _groupLocalService.getUserGroupGroup(
+						serviceContext.getCompanyId(),
+						userGroup.getUserGroupId());
+
+					groupIds.add(group.getGroupId());
 				}
 			}
 
