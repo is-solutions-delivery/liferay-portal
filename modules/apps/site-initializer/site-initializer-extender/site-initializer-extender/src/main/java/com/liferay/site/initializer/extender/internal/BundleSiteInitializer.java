a170579642bbc0e718a9f69343415502f329990d
Nilton Vieira <nilton.vieira@liferay.com>
2022-11-28 11:27:25 -0300

    LPS-167118 rename and SF

--- modules/apps/site-initializer/site-initializer-extender/site-initializer-extender/src/main/java/com/liferay/site/initializer/extender/internal/BundleSiteInitializer.java
+++ modules/apps/site-initializer/site-initializer-extender/site-initializer-extender/src/main/java/com/liferay/site/initializer/extender/internal/BundleSiteInitializer.java
@@ -1056,9 +1056,9 @@ public class BundleSiteInitializer implements SiteInitializer {
 	}
 
 	private void _addObjectAccountEntryRestricted(
-		Map<String, String> listTypeDefinitionIdsStringUtilReplaceValues,
-		ObjectDefinitionResource objectDefinitionResource,
-		ServiceContext serviceContext)
+			Map<String, String> listTypeDefinitionIdsStringUtilReplaceValues,
+			ObjectDefinitionResource objectDefinitionResource,
+			ServiceContext serviceContext)
 		throws Exception {
 
 		Set<String> resourcePaths = _servletContext.getResourcePaths(
@@ -1087,15 +1087,16 @@ public class BundleSiteInitializer implements SiteInitializer {
 				continue;
 			}
 
-			com.liferay.object.model.ObjectDefinition objectDefinitionPublish =
-				_objectDefinitionLocalService.fetchObjectDefinition(
-					serviceContext.getCompanyId(),
-					"C_" + objectDefinition.getName());
+			com.liferay.object.model.ObjectDefinition
+				serviceBuilderObjectDefinition =
+					_objectDefinitionLocalService.fetchObjectDefinition(
+						serviceContext.getCompanyId(),
+						"C_" + objectDefinition.getName());
 
-			if (!objectDefinitionPublish.isApproved()) {
+			if (!serviceBuilderObjectDefinition.isApproved()) {
 				objectDefinition =
 					objectDefinitionResource.patchObjectDefinition(
-						objectDefinitionPublish.getObjectDefinitionId(),
+						serviceBuilderObjectDefinition.getObjectDefinitionId(),
 						objectDefinition);
 
 				objectDefinitionResource.postObjectDefinitionPublish(
