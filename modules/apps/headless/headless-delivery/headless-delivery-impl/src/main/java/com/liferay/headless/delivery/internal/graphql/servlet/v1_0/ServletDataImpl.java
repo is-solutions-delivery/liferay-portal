e2011d14f18165752a497cc10590ad162491db22
Gleice Lisbino <gleice.lisbino@liferay.com>
2022-12-14 09:28:12 -0300

    LPS-166913 buildRest

--- modules/apps/headless/headless-delivery/headless-delivery-impl/src/main/java/com/liferay/headless/delivery/internal/graphql/servlet/v1_0/ServletDataImpl.java
+++ modules/apps/headless/headless-delivery/headless-delivery-impl/src/main/java/com/liferay/headless/delivery/internal/graphql/servlet/v1_0/ServletDataImpl.java
@@ -1826,6 +1826,11 @@ public class ServletDataImpl implements ServletData {
 							MessageBoardMessageResourceImpl.class,
 							"getSiteMessageBoardMessageByFriendlyUrlPath"));
 					put(
+						"query#userMessageBoardMessagesActivity",
+						new ObjectValuePair<>(
+							MessageBoardMessageResourceImpl.class,
+							"getSiteUserMessageBoardMessagesActivityPage"));
+					put(
 						"query#siteMessageBoardMessagePermissions",
 						new ObjectValuePair<>(
 							MessageBoardMessageResourceImpl.class,
