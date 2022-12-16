ca74a23f02b836bd059f404dd8995cc2dfd0fb1e
Gleice Lisbino <gleice.lisbino@liferay.com>
2022-12-16 08:45:23 -0300

    LPS-166913 Regen

--- modules/apps/headless/headless-delivery/headless-delivery-api/src/main/java/com/liferay/headless/delivery/resource/v1_0/MessageBoardMessageResource.java
+++ modules/apps/headless/headless-delivery/headless-delivery-api/src/main/java/com/liferay/headless/delivery/resource/v1_0/MessageBoardMessageResource.java
@@ -176,11 +176,6 @@ public interface MessageBoardMessageResource {
 			Long siteId, String friendlyUrlPath)
 		throws Exception;
 
-	public Page<MessageBoardMessage>
-			getSiteUserMessageBoardMessagesActivityPage(
-				Long siteId, Long userId, Pagination pagination)
-		throws Exception;
-
 	public Page<com.liferay.portal.vulcan.permission.Permission>
 			getSiteMessageBoardMessagePermissionsPage(
 				Long siteId, String roleNames)
@@ -190,6 +185,11 @@ public interface MessageBoardMessageResource {
 			putSiteMessageBoardMessagePermissionsPage(
 				Long siteId,
 				com.liferay.portal.vulcan.permission.Permission[] permissions)
+		throws Exception;
+
+	public Page<MessageBoardMessage>
+			getSiteUserMessageBoardMessagesActivityPage(
+				Long siteId, Long userId, Pagination pagination)
 		throws Exception;
 
 	public default void setContextAcceptLanguage(
