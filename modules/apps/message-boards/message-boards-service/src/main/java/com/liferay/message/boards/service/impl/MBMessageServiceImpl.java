b6cd1f1c6521b84c1265c7ef103707ff2482d10d
Gleice Lisbino <gleice.lisbino@liferay.com>
2022-12-19 12:13:26 -0300

    LPS-166913 SF

--- modules/apps/message-boards/message-boards-service/src/main/java/com/liferay/message/boards/service/impl/MBMessageServiceImpl.java
+++ modules/apps/message-boards/message-boards-service/src/main/java/com/liferay/message/boards/service/impl/MBMessageServiceImpl.java
@@ -709,7 +709,6 @@ public class MBMessageServiceImpl extends MBMessageServiceBaseImpl {
 			long userId, long groupId, int start, int end)
 		throws PortalException {
 
-
 		_userPermission.check(getPermissionChecker(), userId, ActionKeys.VIEW);
 
 		return mbMessageLocalService.getSiteUserMessageBoardMessagesActivity(
@@ -1092,6 +1091,7 @@ public class MBMessageServiceImpl extends MBMessageServiceBaseImpl {
 
 	@Reference
 	private SyndModelFactory _syndModelFactory;
+
 	@Reference
 	private UserPermission _userPermission;
 
