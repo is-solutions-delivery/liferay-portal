2156a3cae394d517c7e378dc3233a9c3f6318fd5
Nilton Vieira <nilton.vieira@liferay.com>
2022-12-29 16:46:31 -0300

    LPS-166909 buildREST

--- modules/apps/headless/headless-delivery/headless-delivery-impl/src/main/java/com/liferay/headless/delivery/internal/graphql/servlet/v1_0/ServletDataImpl.java
+++ modules/apps/headless/headless-delivery/headless-delivery-impl/src/main/java/com/liferay/headless/delivery/internal/graphql/servlet/v1_0/ServletDataImpl.java
@@ -1846,6 +1846,11 @@ public class ServletDataImpl implements ServletData {
 							MessageBoardSectionResourceImpl.class,
 							"getMessageBoardSectionMessageBoardSectionsPage"));
 					put(
+						"query#messageBoardSectionByFriendlyUrlPath",
+						new ObjectValuePair<>(
+							MessageBoardSectionResourceImpl.class,
+							"getSiteMessageBoardSectionByFriendlyUrlPath"));
+					put(
 						"query#messageBoardSections",
 						new ObjectValuePair<>(
 							MessageBoardSectionResourceImpl.class,
