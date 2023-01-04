09e99ce69b765f4e50fd153e004931d687c67472
Tamyris Torres <tamyris.bernardo@liferay.com>
2023-01-04 16:24:55 -0300

    LPS-167118 SF

--- modules/apps/site-initializer/site-initializer-extender/site-initializer-extender/src/main/java/com/liferay/site/initializer/extender/internal/BundleSiteInitializer.java
+++ modules/apps/site-initializer/site-initializer-extender/site-initializer-extender/src/main/java/com/liferay/site/initializer/extender/internal/BundleSiteInitializer.java
@@ -4630,11 +4630,12 @@ public class BundleSiteInitializer implements SiteInitializer {
 
 			com.liferay.object.model.ObjectDefinition
 				serviceBuilderObjectDefinition =
-				_objectDefinitionLocalService.fetchObjectDefinition(
-					serviceContext.getCompanyId(), "C_" + entry.getKey());
+					_objectDefinitionLocalService.fetchObjectDefinition(
+						serviceContext.getCompanyId(), "C_" + entry.getKey());
 
-			_objectDefinitionLocalService.restrictObjectDefinitionByAccountEntry(
-				serviceBuilderObjectDefinition, serviceContext.getUserId());
+			_objectDefinitionLocalService.
+				restrictObjectDefinitionByAccountEntry(
+					serviceBuilderObjectDefinition, serviceContext.getUserId());
 		}
 	}
 
