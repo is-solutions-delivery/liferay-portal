64d3fbda5ddce47725f8d6d1b455c66211a2ce52
José Abelenda <jose.abelenda@liferay.com>
2023-01-09 14:57:31 -0300

    LPS-167118 Rename

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectDefinitionLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectDefinitionLocalServiceImpl.java
@@ -600,7 +600,7 @@ public class ObjectDefinitionLocalServiceImpl
 			objectDefinitionLocalService.fetchObjectDefinition(
 				objectDefinition.getCompanyId(), "AccountEntry");
 
-		String nameObjectRelationship =
+		String objectRelationshipName =
 			StringUtil.toLowerCase(accountEntryObjectDefinition.getName()) +
 				objectDefinition.getShortName();
 
@@ -609,8 +609,8 @@ public class ObjectDefinitionLocalServiceImpl
 				userId, accountEntryObjectDefinition.getObjectDefinitionId(),
 				objectDefinition.getObjectDefinitionId(), 0,
 				ObjectRelationshipConstants.DELETION_TYPE_CASCADE,
-				LocalizedMapUtil.getLocalizedMap(nameObjectRelationship),
-				nameObjectRelationship,
+				LocalizedMapUtil.getLocalizedMap(objectRelationshipName),
+				objectRelationshipName,
 				ObjectRelationshipConstants.TYPE_ONE_TO_MANY);
 
 		objectDefinition.setAccountEntryRestrictedObjectFieldId(
