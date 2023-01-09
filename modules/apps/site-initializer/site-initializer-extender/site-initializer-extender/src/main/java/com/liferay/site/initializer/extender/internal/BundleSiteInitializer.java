f548031ba579f782e196acf6c9484b835c3d8638
José Abelenda <jose.abelenda@liferay.com>
2023-01-09 14:59:42 -0300

    LPS-167118 Inline

--- modules/apps/site-initializer/site-initializer-extender/site-initializer-extender/src/main/java/com/liferay/site/initializer/extender/internal/BundleSiteInitializer.java
+++ modules/apps/site-initializer/site-initializer-extender/site-initializer-extender/src/main/java/com/liferay/site/initializer/extender/internal/BundleSiteInitializer.java
@@ -1196,9 +1196,19 @@ public class BundleSiteInitializer implements SiteInitializer {
 			() -> _addOrUpdateObjectRelationships(
 				objectDefinitionIdsStringUtilReplaceValues, serviceContext));
 
-		_invoke(
-			() -> _updateObjectDefinitionAccountEntryRestricted(
-				accountEntryRestrictedObjectDefinitionMap, serviceContext));
+		for (Map.Entry<String, ObjectDefinition> entry :
+				accountEntryRestrictedObjectDefinitionMap.entrySet()) {
+
+			com.liferay.object.model.ObjectDefinition
+				serviceBuilderObjectDefinition =
+					_objectDefinitionLocalService.fetchObjectDefinition(
+						serviceContext.getCompanyId(), "C_" + entry.getKey());
+
+			_objectDefinitionLocalService.
+				restrictObjectDefinitionByAccountEntry(
+					serviceBuilderObjectDefinition, serviceContext.getUserId());
+		}
+
 		_invoke(
 			() -> _addOrUpdateObjectFields(
 				listTypeDefinitionIdsStringUtilReplaceValues,
@@ -4502,25 +4512,6 @@ public class BundleSiteInitializer implements SiteInitializer {
 			documentsStringUtilReplaceValues, false, serviceContext);
 		_updateLayoutSet(
 			documentsStringUtilReplaceValues, true, serviceContext);
-	}
-
-	private void _updateObjectDefinitionAccountEntryRestricted(
-			Map<String, ObjectDefinition> objectDefinitionMap,
-			ServiceContext serviceContext)
-		throws Exception {
-
-		for (Map.Entry<String, ObjectDefinition> entry :
-				objectDefinitionMap.entrySet()) {
-
-			com.liferay.object.model.ObjectDefinition
-				serviceBuilderObjectDefinition =
-					_objectDefinitionLocalService.fetchObjectDefinition(
-						serviceContext.getCompanyId(), "C_" + entry.getKey());
-
-			_objectDefinitionLocalService.
-				restrictObjectDefinitionByAccountEntry(
-					serviceBuilderObjectDefinition, serviceContext.getUserId());
-		}
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(
