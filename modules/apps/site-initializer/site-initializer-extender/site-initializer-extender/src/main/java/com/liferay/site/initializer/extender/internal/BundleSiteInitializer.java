b3fe52a2d0ccc61b7d5291c1ad2086cb01131077
Tamyris Torres <tamyris.bernardo@liferay.com>
2023-02-01 14:55:45 -0300

    LPS-167118 Fix

--- modules/apps/site-initializer/site-initializer-extender/site-initializer-extender/src/main/java/com/liferay/site/initializer/extender/internal/BundleSiteInitializer.java
+++ modules/apps/site-initializer/site-initializer-extender/site-initializer-extender/src/main/java/com/liferay/site/initializer/extender/internal/BundleSiteInitializer.java
@@ -1247,7 +1247,7 @@ public class BundleSiteInitializer implements SiteInitializer {
 				objectDefinitionsPage.fetchFirstItem();
 
 			if (existingObjectDefinition == null) {
-				if (objectDefinition.getAccountEntryRestricted()) {
+				if (Validator.isNotNull(objectDefinition.getAccountEntryRestricted())) {
 					accountEntryRestrictedObjectDefinitionMap.put(
 						objectDefinition.getName(), objectDefinition);
 				}
