726cec8cc1a25ceed592e3cfb45ed9b3b0e7c4b8
Keven Leone <keven.leone@liferay.com>
2023-02-01 11:49:16 -0300

    LPS-166909 Redirect and read friedlyUrlPath

--- modules/apps/questions/questions-web/src/main/resources/META-INF/resources/js/pages/questions/Questions.es.js
+++ modules/apps/questions/questions-web/src/main/resources/META-INF/resources/js/pages/questions/Questions.es.js
@@ -157,8 +157,7 @@ export default withRouter(({history, location, match: {params}}) => {
 							? MAX_NUMBER_OF_QUESTIONS
 							: messageBoardThreads.totalCount,
 				});
-			}
-			catch (error) {
+			} catch (error) {
 				if (process.env.NODE_ENV === 'development') {
 					console.error(error);
 				}
