67cdfeed97f9dceff2e38a983a8706fd6dffafe3
Nilton Vieira <nilton.vieira@liferay.com>
2023-03-03 16:40:27 -0300

    LPS-177489 no need

--- modules/apps/site-initializer/site-initializer-extender/site-initializer-extender/src/main/java/com/liferay/site/initializer/extender/internal/BundleSiteInitializer.java
+++ modules/apps/site-initializer/site-initializer-extender/site-initializer-extender/src/main/java/com/liferay/site/initializer/extender/internal/BundleSiteInitializer.java
@@ -125,6 +125,7 @@ import com.liferay.portal.kernel.model.UserGroup;
 import com.liferay.portal.kernel.model.role.RoleConstants;
 import com.liferay.portal.kernel.module.configuration.ConfigurationProvider;
 import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.security.auth.PrincipalThreadLocal;
 import com.liferay.portal.kernel.security.permission.ResourceActionsUtil;
 import com.liferay.portal.kernel.service.GroupLocalService;
 import com.liferay.portal.kernel.service.LayoutLocalService;
@@ -412,9 +413,8 @@ public class BundleSiteInitializer implements SiteInitializer {
 		}
 
 		try {
-			Group group = _groupLocalService.getGroup(groupId);
-
-			User user = _userLocalService.getUser(group.getCreatorUserId());
+			User user = _userLocalService.getUser(
+				PrincipalThreadLocal.getUserId());
 
 			ServiceContext serviceContextThreadLocal =
 				ServiceContextThreadLocal.getServiceContext();
