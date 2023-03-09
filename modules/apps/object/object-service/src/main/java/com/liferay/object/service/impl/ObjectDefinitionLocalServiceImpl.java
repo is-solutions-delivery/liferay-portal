ca9151dab23beb1d8bde67584976399ed9d36bb3
Tamyris Torres <tamyris.bernardo@liferay.com>
2023-03-09 17:08:16 -0300

    LPS-167118 Put testObject

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectDefinitionLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectDefinitionLocalServiceImpl.java
@@ -486,7 +486,8 @@ public class ObjectDefinitionLocalServiceImpl
 		ObjectDefinition objectDefinition2 = getObjectDefinition(
 			objectDefinitionId);
 
-		if (objectDefinition2.isAccountEntryRestricted()) {
+		if (objectDefinition2.isAccountEntryRestricted() ||
+			objectDefinition2.isSystem()) {
 			return objectDefinition2;
 		}
 
