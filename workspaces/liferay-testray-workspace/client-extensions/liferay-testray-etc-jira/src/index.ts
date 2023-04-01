9610638ddab4f79ec0d9398b3ef8c638085f04a6
Keven Leone <keven.leone@liferay.com>
2023-04-01 15:19:31 -0300

    LPS-180555 SF

--- workspaces/liferay-testray-workspace/client-extensions/liferay-testray-etc-jira/src/index.ts
+++ workspaces/liferay-testray-workspace/client-extensions/liferay-testray-etc-jira/src/index.ts
@@ -1,8 +1,23 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
 import { Elysia, t } from 'elysia';
+
 import Cache from './lib/Cache';
+import logger from './lib/Logger';
 import Jira from './services/jira/Jira';
 import JiraEngine from './services/jira/JiraEngine';
-import logger from './lib/Logger';
 import Testray from './services/liferay/Testray';
 
 const { APP_DEBUG_CACHE_ROUTER = '/cache', PORT } = Bun.env;
@@ -12,47 +27,47 @@ const jiraEngine = new JiraEngine();
 const testray = new Testray();
 
 const getUserId = (request: Request) =>
-  request.headers.get('liferay-user-id') ?? '20123';
+    request.headers.get('liferay-user-id') ?? '20123';
 
 const cacheInstance = Cache.getInstance();
 
 new Elysia()
-  .get('/', () => 'Testray LXC Jira Integration')
-  .get(APP_DEBUG_CACHE_ROUTER, () => {
-    logger.debug(cacheInstance);
+    .get('/', () => 'Testray LXC Jira Integration')
+    .get(APP_DEBUG_CACHE_ROUTER, () => {
+        logger.debug(cacheInstance);
 
-    return cacheInstance;
-  })
-  .get('/jira/authorize', async ({ set, request }) =>
-    jira.authorize(getUserId(request), set)
-  )
-  .get('/jira/authorize/callback', async ({ query: { code, state } }) => {
-    const response = await jira.exchangeAuthorizationCode({
-      code: code as string,
-      state: state as string,
-    });
-
-    await testray.setTestrayOAuthJiraCode(response, state as string);
-
-    return response;
-  })
-  .put(
-    '/jira/ticket',
-    ({ request, body }) =>
-      jiraEngine.updateIssues(body.tickets, getUserId(request)),
-    {
-      schema: {
-        body: t.Object({
-          tickets: t.Array(t.String()),
-        }),
-      },
-    }
-  )
-  .get('/jira/ticket/:ticket', ({ request, params: { ticket } }) =>
-    jira.getIssue(ticket, getUserId(request))
-  )
-  .listen(Number(PORT), ({ hostname, port }) =>
-    logger.info(
-      `🦊 Testray LXC Integration with Elysia is running at ${hostname}:${port}`
+        return cacheInstance;
+    })
+    .get('/jira/authorize', async ({ request, set }) =>
+        jira.authorize(getUserId(request), set)
     )
-  );
+    .get('/jira/authorize/callback', async ({ query: { code, state } }) => {
+        const response = await jira.exchangeAuthorizationCode({
+            code: code as string,
+            state: state as string,
+        });
+
+        await testray.setTestrayOAuthJiraCode(response, state as string);
+
+        return response;
+    })
+    .put(
+        '/jira/ticket',
+        ({ body, request }) =>
+            jiraEngine.updateIssues(body.tickets, getUserId(request)),
+        {
+            schema: {
+                body: t.Object({
+                    tickets: t.Array(t.String()),
+                }),
+            },
+        }
+    )
+    .get('/jira/ticket/:ticket', ({ params: { ticket }, request }) =>
+        jira.getIssue(ticket, getUserId(request))
+    )
+    .listen(Number(PORT), ({ hostname, port }) =>
+        logger.info(
+            `🦊 Testray LXC Integration with Elysia is running at ${hostname}:${port}`
+        )
+    );
--- workspaces/liferay-testray-workspace/client-extensions/liferay-testray-etc-jira/src/services/jira/Jira.ts
+++ workspaces/liferay-testray-workspace/client-extensions/liferay-testray-etc-jira/src/services/jira/Jira.ts
@@ -1,70 +1,84 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
 import Cache from '../../lib/Cache';
-import JiraAuth from './JiraAuth';
 import logger from '../../lib/Logger';
+import JiraAuth from './JiraAuth';
 
 const {
-  JIRA_API_BASE_URL,
-  JIRA_APP_NAME,
-  JIRA_AUTH_BASE_URL,
-  JIRA_AUTH_CLIENT_ID,
-  JIRA_AUTH_CLIENT_SECRET,
-  JIRA_AUTH_GRANT_TYPE_REFRESH_TOKEN,
-  JIRA_AUTH_GRANT_TYPE,
-  JIRA_AUTH_REDIRECT_URI,
-  JIRA_AUTH_SCOPES,
-  JIRA_AUTH_STATE_PREFIX,
+    JIRA_API_BASE_URL,
+    JIRA_APP_NAME,
+    JIRA_AUTH_BASE_URL,
+    JIRA_AUTH_CLIENT_ID,
+    JIRA_AUTH_CLIENT_SECRET,
+    JIRA_AUTH_GRANT_TYPE,
+    JIRA_AUTH_GRANT_TYPE_REFRESH_TOKEN,
+    JIRA_AUTH_REDIRECT_URI,
+    JIRA_AUTH_SCOPES,
+    JIRA_AUTH_STATE_PREFIX,
 } = Bun.env;
 
 const cacheInstance = Cache.getInstance();
 
 logger.debug({
-  cacheInstance,
-  JIRA_API_BASE_URL,
-  JIRA_APP_NAME,
-  JIRA_AUTH_BASE_URL,
-  JIRA_AUTH_CLIENT_ID,
-  JIRA_AUTH_CLIENT_SECRET,
-  JIRA_AUTH_GRANT_TYPE_REFRESH_TOKEN,
-  JIRA_AUTH_GRANT_TYPE,
-  JIRA_AUTH_REDIRECT_URI,
-  JIRA_AUTH_SCOPES,
-  JIRA_AUTH_STATE_PREFIX,
+    JIRA_API_BASE_URL,
+    JIRA_APP_NAME,
+    JIRA_AUTH_BASE_URL,
+    JIRA_AUTH_CLIENT_ID,
+    JIRA_AUTH_CLIENT_SECRET,
+    JIRA_AUTH_GRANT_TYPE,
+    JIRA_AUTH_GRANT_TYPE_REFRESH_TOKEN,
+    JIRA_AUTH_REDIRECT_URI,
+    JIRA_AUTH_SCOPES,
+    JIRA_AUTH_STATE_PREFIX,
+    cacheInstance,
 });
 
 class Jira extends JiraAuth {
-  public async getIssue(ticket: string, userId: string) {
-    const { cloudId, token } = await this.getTokenAndCloudId(userId);
+    public async getIssue(ticket: string, userId: string) {
+        const { cloudId, token } = await this.getTokenAndCloudId(userId);
 
-    const response = await fetch(
-      `${JIRA_API_BASE_URL}/ex/jira/${cloudId}/rest/api/latest/issue/${ticket}`,
-      {
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: 'Bearer ' + token,
-        },
-      }
-    );
+        const response = await fetch(
+            `${JIRA_API_BASE_URL}/ex/jira/${cloudId}/rest/api/latest/issue/${ticket}`,
+            {
+                headers: {
+                    "Authorization": `Bearer ${token}`,
+                    'Content-Type': 'application/json',
+                },
+            }
+        );
 
-    return response.json();
-  }
+        return response.json();
+    }
 
-  public async updateIssue(ticket: string, body: unknown, userId: string) {
-    const { cloudId, token } = await this.getTokenAndCloudId(userId);
+    public async updateIssue(ticket: string, body: unknown, userId: string) {
+        const { cloudId, token } = await this.getTokenAndCloudId(userId);
 
-    const response = await fetch(
-      `${JIRA_API_BASE_URL}/ex/jira/${cloudId}/rest/api/latest/issue/${ticket}`,
-      {
-        body: JSON.stringify(body),
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: 'Bearer ' + token,
-        },
-        method: 'PUT',
-      }
-    );
+        const response = await fetch(
+            `${JIRA_API_BASE_URL}/ex/jira/${cloudId}/rest/api/latest/issue/${ticket}`,
+            {
+                body: JSON.stringify(body),
+                headers: {
+                    "Authorization": `Bearer ${token}`,
+                    'Content-Type': 'application/json',
+                },
+                method: 'PUT',
+            }
+        );
 
-    return response.json();
-  }
+        return response.json();
+    }
 }
 
 export default Jira;
