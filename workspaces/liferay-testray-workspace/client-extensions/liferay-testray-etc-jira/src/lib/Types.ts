f47bc24d56f5e0c613d8bbc91a91dc41887de77f
Keven Leone <keven.leone@liferay.com>
2023-04-01 15:19:31 -0300

    LPS-180555 SF

--- workspaces/liferay-testray-workspace/client-extensions/liferay-testray-etc-jira/src/lib/Types.ts
+++ workspaces/liferay-testray-workspace/client-extensions/liferay-testray-etc-jira/src/lib/Types.ts
@@ -1,45 +1,58 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
 export interface APIResponse<T = any> {
-  items: T[];
-  totalCount: number;
+    items: T[];
+    totalCount: number;
 }
 
 export interface JiraAuthorizeCallback {
-  code: string;
-  expiresIn?: number;
-  state: string;
+    code: string;
+    expiresIn?: number;
+    state: string;
 }
 
 export interface JiraAuthorizePayload {
-  access_token: string;
-  expires_in: number;
-  refresh_token: string;
-  scope: string;
+    access_token: string;
+    expires_in: number;
+    refresh_token: string;
+    scope: string;
 }
 
 export type JiraAccessibleResponse = {
-  id: string;
-  name: string;
-  scopes: string[];
-  url: string;
+    id: string;
+    name: string;
+    scopes: string[];
+    url: string;
 }[];
 
 export interface TestrayJiraOAuth {
-  expiresIn: number;
-  id: number;
-  accessToken: string;
-  r_testrayJiraOAuth_userId: number;
-  refreshToken: string;
+    accessToken: string;
+    expiresIn: number;
+    id: number;
+    r_testrayJiraOAuth_userId: number;
+    refreshToken: string;
 }
 
 export interface TestrayIssue {
-  id: string;
-  issueToCaseResultsIssues: {
-    r_caseResultToCaseResultsIssues_c_caseResult: {
-      r_caseToCaseResult_c_case: {
-        priority: number;
-        name: string;
-      };
-    };
-  }[];
-  name: string;
+    id: string;
+    issueToCaseResultsIssues: {
+        r_caseResultToCaseResultsIssues_c_caseResult: {
+            r_caseToCaseResult_c_case: {
+                name: string;
+                priority: number;
+            };
+        };
+    }[];
+    name: string;
 }
--- workspaces/liferay-testray-workspace/client-extensions/liferay-testray-etc-jira/src/services/liferay/LiferayAuth.ts
+++ workspaces/liferay-testray-workspace/client-extensions/liferay-testray-etc-jira/src/services/liferay/LiferayAuth.ts
@@ -1,34 +1,48 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
 import { baseFetcher } from '../../lib/Fetch';
 
 const {
-  LIFERAY_AUTH_CLIENT_ID,
-  LIFERAY_AUTH_CLIENT_SECRET,
-  LIFERAY_AUTH_TOKEN,
-  LIFERAY_BASE_URL,
+    LIFERAY_AUTH_CLIENT_ID,
+    LIFERAY_AUTH_CLIENT_SECRET,
+    LIFERAY_AUTH_TOKEN,
+    LIFERAY_BASE_URL,
 } = Bun.env;
 
 class LiferayAuth {
-  protected fetcher = baseFetcher(LIFERAY_BASE_URL as string, {
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: LIFERAY_AUTH_TOKEN as string,
-    },
-  });
-
-  public async getAuthToken() {
-    const response = await this.fetcher('/o/oauth2/token', {
-      body: JSON.stringify({
-        client_id: LIFERAY_AUTH_CLIENT_ID,
-        client_secret: LIFERAY_AUTH_CLIENT_SECRET,
-        grant_type: 'client_credentials',
-      }),
+    protected fetcher = baseFetcher(LIFERAY_BASE_URL as string, {
+        headers: {
+            "Authorization": LIFERAY_AUTH_TOKEN as string,
+            'Content-Type': 'application/json',
+        },
     });
 
-    return {
-      access_token: response.access_token,
-      expires_in: response.expires_in,
-    };
-  }
+    public async getAuthToken() {
+        const response = await this.fetcher('/o/oauth2/token', {
+            body: JSON.stringify({
+                client_id: LIFERAY_AUTH_CLIENT_ID,
+                client_secret: LIFERAY_AUTH_CLIENT_SECRET,
+                grant_type: 'client_credentials',
+            }),
+        });
+
+        return {
+            access_token: response.access_token,
+            expires_in: response.expires_in,
+        };
+    }
 }
 
 export default LiferayAuth;
