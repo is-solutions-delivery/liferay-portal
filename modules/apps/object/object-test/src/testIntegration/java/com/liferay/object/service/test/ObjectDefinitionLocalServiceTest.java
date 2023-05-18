ea1d17fbf5890c7bb497d63ba0588e1739e825ff
Tamyris Torres <tamyris.bernardo@liferay.com>
2023-05-18 18:14:28 -0300

    LPS-183396 Rename

--- modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/service/test/ObjectDefinitionLocalServiceTest.java
+++ modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/service/test/ObjectDefinitionLocalServiceTest.java
@@ -1070,7 +1070,7 @@ public class ObjectDefinitionLocalServiceTest {
 				ObjectRelationshipConstants.TYPE_ONE_TO_MANY);
 
 		objectDefinition2 =
-			_objectDefinitionLocalService.enableAccountEntryRestricted(
+			_objectDefinitionLocalService.enableObjectRestricted(
 				objectRelationship);
 
 		Assert.assertTrue(objectDefinition2.isAccountEntryRestricted());
@@ -1093,7 +1093,7 @@ public class ObjectDefinitionLocalServiceTest {
 				ObjectRelationshipConstants.TYPE_ONE_TO_MANY);
 
 		try {
-			_objectDefinitionLocalService.enableAccountEntryRestricted(
+			_objectDefinitionLocalService.enableObjectRestricted(
 				objectRelationship);
 
 			Assert.fail();
--- modules/apps/site-initializer/site-initializer-extender/site-initializer-extender/src/main/java/com/liferay/site/initializer/extender/internal/BundleSiteInitializer.java
+++ modules/apps/site-initializer/site-initializer-extender/site-initializer-extender/src/main/java/com/liferay/site/initializer/extender/internal/BundleSiteInitializer.java
@@ -4398,7 +4398,7 @@ public class BundleSiteInitializer implements SiteInitializer {
 						jsonObject.getString("objectField"));
 			}
 
-				_objectDefinitionLocalService.enableAccountEntryRestricted(
+				_objectDefinitionLocalService.enableObjectRestricted(
 					objectDefinition2, objectField);
 		}
 	}
