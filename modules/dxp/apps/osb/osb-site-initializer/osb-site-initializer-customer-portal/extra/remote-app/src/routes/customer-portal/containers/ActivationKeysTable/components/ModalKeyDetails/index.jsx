45239a236242be414d3cb7ee59c0556a1d997a1a
Keven Leone <keven.leone@liferay.com>
2023-05-21 22:36:20 -0300

    LPS-184898 SF

--- modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/routes/customer-portal/containers/ActivationKeysTable/components/ModalKeyDetails/index.jsx
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/routes/customer-portal/containers/ActivationKeysTable/components/ModalKeyDetails/index.jsx
@@ -27,6 +27,12 @@ import {ALERT_ACTIVATION_AGGREGATED_KEYS_DOWNLOAD_TEXT} from '../../utils/consta
 import {downloadActivationLicenseKey} from '../../utils/downloadActivationLicenseKey';
 import TableKeyDetails from '../TableKeyDetails';
 
+const openToast = (title, {type = 'success'} = {}) =>
+	Liferay.Util.openToast({
+		title: i18n.translate(title),
+		type,
+	});
+
 const ModalKeyDetails = ({
 	currentActivationKey,
 	observer,
@@ -35,7 +41,7 @@ const ModalKeyDetails = ({
 	sessionId,
 }) => {
 	const {provisioningServerAPI} = useAppPropertiesContext();
-	const [valueToCopyToClipboard, setValueToCopyToClipboard] = useState('');
+	const [clipboardValue, setClipboardValue] = useState('');
 	const [isLoading, setIsLoading] = useState(false);
 	const [
 		activationKeysDownloadStatusModal,
@@ -54,175 +60,166 @@ const ModalKeyDetails = ({
 
 	const {featureFlags} = useAppPropertiesContext();
 
-	const responseMessage = (Message, typeMessage) =>
-		Liferay.Util.openToast({
-			title: i18n.translate(Message),
-			type: typeMessage,
-		});
-
-	const getStatusSubscription = async () => {
-		try {
-			const result = await getSubscriptionInKey(
-				provisioningServerAPI,
-				currentActivationKey.id,
-				sessionId
-			);
-			setToggleSubscription(result);
-			setIsLoading(true);
-			setHasErrorSubscription(false);
-		} catch {
-			setIsLoading(true);
-			responseMessage('get-subscription-failed', 'danger');
-			setHasErrorSubscription(true);
-		}
-	};
-
 	useEffect(() => {
-		getStatusSubscription();
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, []);
+		setIsLoading(true);
 
-	const handleSubscriptionInKey = (status) => {
-		const handleToggle = () => setToggleSubscription((toggled) => !toggled);
+		getSubscriptionInKey(
+			provisioningServerAPI,
+			currentActivationKey.id,
+			sessionId
+		)
+			.then((result) => {
+				setToggleSubscription(result);
+				setHasErrorSubscription(false);
+			})
+			.catch(() => {
+				openToast('get-subscription-failed', {type: 'danger'});
+				setHasErrorSubscription(true);
+			})
+			.finally(() => {
+				setIsLoading(false);
+			});
+	}, [currentActivationKey.id, provisioningServerAPI, sessionId]);
+
+	const handleToggle = () => setToggleSubscription((toggled) => !toggled);
+
+	const handleSubscriptionInKey = async (status) => {
 		handleToggle();
+
 		const fn = status ? deleteSubscriptionInKey : putSubscriptionInKey;
+
 		try {
-			fn(provisioningServerAPI, currentActivationKey.id, sessionId);
-			responseMessage('success', 'success');
+			await fn(provisioningServerAPI, currentActivationKey.id, sessionId);
+
+			openToast('success');
 		} catch {
 			setTimeout(() => {
 				handleToggle();
-				responseMessage('subscription-failed', 'danger');
+				openToast('subscription-failed', {type: 'danger'});
 			}, 500);
 		}
 	};
 
 	return (
-		isLoading && (
-			<ClayModal center observer={observer} size="lg">
-				<div className="pt-4 px-4">
-					<div className="d-flex justify-content-between mb-4">
-						<div className="flex-row mb-1">
-							<h6 className="text-brand-primary">
-								{i18n.translate('activation-key-details')}
-							</h6>
+		<ClayModal center observer={observer} size="lg">
+			<div className="pt-4 px-4">
+				<div className="d-flex justify-content-between mb-4">
+					<div className="flex-row mb-1">
+						<h6 className="text-brand-primary">
+							{i18n.translate('activation-key-details')}
+						</h6>
 
-							<h2 className="text-neutral-10">
-								{currentActivationKey.name}
-							</h2>
+						<h2 className="text-neutral-10">
+							{currentActivationKey.name}
+						</h2>
 
-							<p>{currentActivationKey.description}</p>
-						</div>
-
-						<Button
-							appendIcon="times"
-							aria-label="close"
-							className="align-self-start"
-							displayType="unstyled"
-							onClick={onClose}
-						/>
+						<p>{currentActivationKey.description}</p>
 					</div>
 
-					<TableKeyDetails
-						currentActivationKey={currentActivationKey}
-						setValueToCopyToClipboard={setValueToCopyToClipboard}
+					<Button
+						appendIcon="times"
+						aria-label="close"
+						className="align-self-start"
+						displayType="unstyled"
+						onClick={onClose}
 					/>
-
-					{featureFlags.includes('LPS-185063') && (
-						<>
-							<div className="dropdown-divider"></div>
-
-							<div>
-								<ClayToggle
-									disabled={hasErrorSubscription}
-									label={i18n.sub('expiration-notifications')}
-									onClick={() =>
-										handleSubscriptionInKey(
-											toggledSubscription
-										)
-									}
-									toggled={toggledSubscription}
-								/>
-
-								<p className="pt-2">
-									{i18n.sub(
-										'enable-notifications-through-email-when-this-activation-key-is-about-to-expire-x-days-before-x-days-before-and-on-the-day-of-expiration-you-can-unsubscribe-at-any-time',
-										[30, 15]
-									)}
-								</p>
-							</div>
-
-							<div className="dropdown-divider"></div>
-						</>
-					)}
-
-					<div className="d-flex justify-content-end my-4">
-						<Button displayType="secondary" onClick={onClose}>
-							{i18n.translate('close')}
-						</Button>
-
-						<Button
-							appendIcon="download"
-							className="ml-2"
-							onClick={async () => {
-								const isAbleToDownloadKey = await downloadActivationLicenseKey(
-									currentActivationKey.id,
-									provisioningServerAPI,
-									sessionId,
-									currentActivationKey.productName,
-									currentActivationKey.productVersion,
-									project.name
-								);
-								handleAlertStatus(isAbleToDownloadKey);
-							}}
-						>
-							{i18n.translate('download-key')}
-						</Button>
-					</div>
 				</div>
 
-				{valueToCopyToClipboard && (
-					<ClayAlert.ToastContainer>
-						<ClayAlert
-							autoClose={AUTO_CLOSE_ALERT_TIME.success}
-							displayType="success"
-							onClose={() => setValueToCopyToClipboard(false)}
-						>
-							{i18n.sub('x-copied-to-clipboard', [
-								valueToCopyToClipboard,
-							])}
-						</ClayAlert>
-					</ClayAlert.ToastContainer>
+				<TableKeyDetails
+					currentActivationKey={currentActivationKey}
+					setValueToCopyToClipboard={setClipboardValue}
+				/>
+
+				{featureFlags.includes('LPS-185063') && !isLoading && (
+					<>
+						<div className="dropdown-divider"></div>
+
+						<div>
+							<ClayToggle
+								disabled={hasErrorSubscription}
+								label={i18n.sub('expiration-notifications')}
+								onClick={() =>
+									handleSubscriptionInKey(toggledSubscription)
+								}
+								toggled={toggledSubscription}
+							/>
+
+							<p className="pt-2">
+								{i18n.sub(
+									'enable-notifications-through-email-when-this-activation-key-is-about-to-expire-x-days-before-x-days-before-and-on-the-day-of-expiration-you-can-unsubscribe-at-any-time',
+									[30, 15]
+								)}
+							</p>
+						</div>
+
+						<div className="dropdown-divider"></div>
+					</>
 				)}
 
-				{activationKeysDownloadStatusModal && (
-					<ClayAlert.ToastContainer>
-						<ClayAlert
-							autoClose={
-								AUTO_CLOSE_ALERT_TIME[
-									activationKeysDownloadStatusModal
-								]
-							}
-							className="cp-activation-key-download-alert"
-							displayType={
-								ALERT_DOWNLOAD_TYPE[
-									activationKeysDownloadStatusModal
-								]
-							}
-							onClose={() =>
-								setActivationKeysDownloadStatusModal('')
-							}
-						>
-							{
-								ALERT_ACTIVATION_AGGREGATED_KEYS_DOWNLOAD_TEXT[
-									activationKeysDownloadStatusModal
-								]
-							}
-						</ClayAlert>
-					</ClayAlert.ToastContainer>
-				)}
-			</ClayModal>
-		)
+				<div className="d-flex justify-content-end my-4">
+					<Button displayType="secondary" onClick={onClose}>
+						{i18n.translate('close')}
+					</Button>
+
+					<Button
+						appendIcon="download"
+						className="ml-2"
+						onClick={async () => {
+							const isAbleToDownloadKey = await downloadActivationLicenseKey(
+								currentActivationKey.id,
+								provisioningServerAPI,
+								sessionId,
+								currentActivationKey.productName,
+								currentActivationKey.productVersion,
+								project.name
+							);
+
+							handleAlertStatus(isAbleToDownloadKey);
+						}}
+					>
+						{i18n.translate('download-key')}
+					</Button>
+				</div>
+			</div>
+
+			{clipboardValue && (
+				<ClayAlert.ToastContainer>
+					<ClayAlert
+						autoClose={AUTO_CLOSE_ALERT_TIME.success}
+						displayType="success"
+						onClose={() => setClipboardValue(false)}
+					>
+						{i18n.sub('x-copied-to-clipboard', [clipboardValue])}
+					</ClayAlert>
+				</ClayAlert.ToastContainer>
+			)}
+
+			{activationKeysDownloadStatusModal && (
+				<ClayAlert.ToastContainer>
+					<ClayAlert
+						autoClose={
+							AUTO_CLOSE_ALERT_TIME[
+								activationKeysDownloadStatusModal
+							]
+						}
+						className="cp-activation-key-download-alert"
+						displayType={
+							ALERT_DOWNLOAD_TYPE[
+								activationKeysDownloadStatusModal
+							]
+						}
+						onClose={() => setActivationKeysDownloadStatusModal('')}
+					>
+						{
+							ALERT_ACTIVATION_AGGREGATED_KEYS_DOWNLOAD_TEXT[
+								activationKeysDownloadStatusModal
+							]
+						}
+					</ClayAlert>
+				</ClayAlert.ToastContainer>
+			)}
+		</ClayModal>
 	);
 };
+
 export default ModalKeyDetails;
