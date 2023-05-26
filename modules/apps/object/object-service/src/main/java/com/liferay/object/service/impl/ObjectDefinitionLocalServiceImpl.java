ff122400b98e66c51af2d604d6489f6a50cc2302
Nilton Vieira <nilton.vieira@liferay.com>
2023-05-26 09:51:48 -0300

    LPS-183396 improve tests

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectDefinitionLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectDefinitionLocalServiceImpl.java
@@ -527,8 +527,8 @@ public class ObjectDefinitionLocalServiceImpl
 			ObjectField objectField)
 		throws PortalException {
 
-		if (!Objects.equals(objectField.getBusinessType(), "Integer") ||
-			!Objects.equals(objectField.getBusinessType(), "LongInteger") ||
+		if (!Objects.equals(objectField.getBusinessType(), "Integer") &&
+			!Objects.equals(objectField.getBusinessType(), "LongInteger") &&
 			!Objects.equals(objectField.getBusinessType(), "Text")) {
 
 			throw new ObjectDefinitionAccountEntryRestrictedException(
@@ -536,7 +536,7 @@ public class ObjectDefinitionLocalServiceImpl
 					"Integer, Long Integer or Text field");
 		}
 
-		ObjectDefinition objectDefinition = objectField.getObjectDefinition();
+		ObjectDefinition objectDefinition = getObjectDefinition(objectField.getObjectDefinitionId());
 
 		if (objectDefinition.isDefaultStorageType() ||
 			objectDefinition.isAccountEntryRestricted()) {
