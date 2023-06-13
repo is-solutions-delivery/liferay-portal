68b88a53db77c744a8163379207d996ecb5c8071
João Victor Damasceno <joao_damasceno2001@hotmail.com>
2023-06-13 10:39:14 -0300

    LPS-186204 Refactor code from test accounts

--- modules/apps/site-initializer/site-initializer-extender/site-initializer-extender-test/src/testIntegration/java/com/liferay/site/initializer/extender/internal/test/BundleSiteInitializerTest.java
+++ modules/apps/site-initializer/site-initializer-extender/site-initializer-extender-test/src/testIntegration/java/com/liferay/site/initializer/extender/internal/test/BundleSiteInitializerTest.java
@@ -195,22 +195,6 @@ import com.liferay.site.navigation.service.SiteNavigationMenuLocalService;
 import com.liferay.style.book.model.StyleBookEntry;
 import com.liferay.style.book.service.StyleBookEntryLocalService;
 import com.liferay.template.model.TemplateEntry;
-
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.Serializable;
-
-import java.math.BigDecimal;
-
-import java.util.Arrays;
-import java.util.Dictionary;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-
-import javax.servlet.ServletContext;
-
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -218,16 +202,26 @@ import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.FrameworkUtil;
 import org.osgi.framework.InvalidSyntaxException;
 import org.osgi.service.cm.Configuration;
 import org.osgi.service.cm.ConfigurationAdmin;
-
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
+
+import javax.servlet.ServletContext;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.Dictionary;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 /**
  * @author Brian Wing Shun Chan
@@ -475,26 +469,26 @@ public class BundleSiteInitializerTest {
 			_serviceContext.fetchUser()
 		).build();
 
-		Account account1 = accountResource.getAccountByExternalReferenceCode(
+		Account account = accountResource.getAccountByExternalReferenceCode(
 			"TESTACCOUNT1");
 
-		Assert.assertNotNull(account1);
-		Assert.assertEquals("Test Account 1", account1.getName());
-		Assert.assertEquals("business", account1.getTypeAsString());
+		Assert.assertNotNull(account);
+		Assert.assertEquals("Test Account 1", account.getName());
+		Assert.assertEquals("business", account.getTypeAsString());
 
-		Account account2 = accountResource.getAccountByExternalReferenceCode(
+		 account = accountResource.getAccountByExternalReferenceCode(
 			"TESTACCOUNT2");
 
-		Assert.assertNotNull(account2);
-		Assert.assertEquals("Test Account 2", account2.getName());
-		Assert.assertEquals("guest", account2.getTypeAsString());
+		Assert.assertNotNull(account);
+		Assert.assertEquals("Test Account 2", account.getName());
+		Assert.assertEquals("guest", account.getTypeAsString());
 
-		Account account3 = accountResource.getAccountByExternalReferenceCode(
+		 account = accountResource.getAccountByExternalReferenceCode(
 			"TESTACCOUNT3");
 
-		Assert.assertNotNull(account3);
-		Assert.assertEquals("Test Account 3", account3.getName());
-		Assert.assertEquals("person", account3.getTypeAsString());
+		Assert.assertNotNull(account);
+		Assert.assertEquals("Test Account 3", account.getName());
+		Assert.assertEquals("person", account.getTypeAsString());
 	}
 
 	private void _assertAccounts2() throws Exception {
@@ -505,33 +499,33 @@ public class BundleSiteInitializerTest {
 			_serviceContext.fetchUser()
 		).build();
 
-		Account account1 = accountResource.getAccountByExternalReferenceCode(
+		Account account = accountResource.getAccountByExternalReferenceCode(
 			"TESTACCOUNT1");
 
-		Assert.assertNotNull(account1);
-		Assert.assertEquals("Test Account 1", account1.getName());
-		Assert.assertEquals("business", account1.getTypeAsString());
+		Assert.assertNotNull(account);
+		Assert.assertEquals("Test Account 1", account.getName());
+		Assert.assertEquals("business", account.getTypeAsString());
 
-		Account account2 = accountResource.getAccountByExternalReferenceCode(
+		account = accountResource.getAccountByExternalReferenceCode(
 			"TESTACCOUNT2");
 
-		Assert.assertNotNull(account2);
-		Assert.assertEquals("Test Account Guest", account2.getName());
-		Assert.assertEquals("guest", account2.getTypeAsString());
+		Assert.assertNotNull(account);
+		Assert.assertEquals("Test Account Guest", account.getName());
+		Assert.assertEquals("guest", account.getTypeAsString());
 
-		Account account3 = accountResource.getAccountByExternalReferenceCode(
+		account = accountResource.getAccountByExternalReferenceCode(
 			"TESTACCOUNT3");
 
-		Assert.assertNotNull(account3);
-		Assert.assertEquals("Test Account 3", account3.getName());
-		Assert.assertEquals("person", account3.getTypeAsString());
+		Assert.assertNotNull(account);
+		Assert.assertEquals("Test Account 3", account.getName());
+		Assert.assertEquals("person", account.getTypeAsString());
 
-		Account account4 = accountResource.getAccountByExternalReferenceCode(
+		account = accountResource.getAccountByExternalReferenceCode(
 			"TESTACCOUNT4");
 
-		Assert.assertNotNull(account4);
-		Assert.assertEquals("Test Account 4", account4.getName());
-		Assert.assertEquals("person", account4.getTypeAsString());
+		Assert.assertNotNull(account);
+		Assert.assertEquals("Test Account 4", account.getName());
+		Assert.assertEquals("person", account.getTypeAsString());
 	}
 
 	private void _assertAssetCategories() throws Exception {
