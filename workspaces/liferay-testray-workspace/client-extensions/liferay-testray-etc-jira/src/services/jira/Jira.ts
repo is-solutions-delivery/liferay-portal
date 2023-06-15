e99f203957ba5a471b72d711d1969c0d7217b060
Wellington Barbosa <welitonpe@gmail.com>
2023-06-15 11:26:41 -0300

    LPS-184593 Created the logic to integrate the Testray with jira
    Co-authored-by: Elvison Victor <elvison.victor@liferay.com>
    Co-authored-by: Eugenio Antunes <eugenio.antunes@liferay.com>

--- workspaces/liferay-testray-workspace/client-extensions/liferay-testray-etc-jira/src/services/jira/Jira.ts
+++ workspaces/liferay-testray-workspace/client-extensions/liferay-testray-etc-jira/src/services/jira/Jira.ts
@@ -12,38 +12,49 @@
  * details.
  */
 
-import Cache from '../../lib/Cache';
-import logger from '../../lib/Logger';
-import JiraAuth from './JiraAuth';
+import Cache from "../../lib/Cache";
+import logger from "../../lib/Logger";
+import JiraAuth from "./JiraAuth";
 
 const {
-    JIRA_API_BASE_URL,
-    JIRA_APP_NAME,
-    JIRA_AUTH_BASE_URL,
-    JIRA_AUTH_CLIENT_ID,
-    JIRA_AUTH_CLIENT_SECRET,
-    JIRA_AUTH_GRANT_TYPE,
-    JIRA_AUTH_GRANT_TYPE_REFRESH_TOKEN,
-    JIRA_AUTH_REDIRECT_URI,
-    JIRA_AUTH_SCOPES,
-    JIRA_AUTH_STATE_PREFIX,
+	JIRA_API_BASE_URL,
+	JIRA_APP_NAME,
+	JIRA_AUTH_BASE_URL,
+	JIRA_AUTH_CLIENT_ID,
+	JIRA_AUTH_CLIENT_SECRET,
+	JIRA_AUTH_GRANT_TYPE,
+	JIRA_AUTH_GRANT_TYPE_REFRESH_TOKEN,
+	JIRA_AUTH_REDIRECT_URI,
+	JIRA_AUTH_SCOPES,
+	JIRA_AUTH_STATE_PREFIX,
+	LIFERAY_TESTRAY_REDIRECT_AUTHORIZATION,
 } = Bun.env;
 
 const cacheInstance = Cache.getInstance();
 
 logger.debug({
-    JIRA_API_BASE_URL,
-    JIRA_APP_NAME,
-    JIRA_AUTH_BASE_URL,
-    JIRA_AUTH_CLIENT_ID,
-    JIRA_AUTH_CLIENT_SECRET,
-    JIRA_AUTH_GRANT_TYPE,
-    JIRA_AUTH_GRANT_TYPE_REFRESH_TOKEN,
-    JIRA_AUTH_REDIRECT_URI,
-    JIRA_AUTH_SCOPES,
-    JIRA_AUTH_STATE_PREFIX,
-    cacheInstance,
+	JIRA_API_BASE_URL,
+	JIRA_APP_NAME,
+	JIRA_AUTH_BASE_URL,
+	JIRA_AUTH_CLIENT_ID,
+	JIRA_AUTH_CLIENT_SECRET,
+	JIRA_AUTH_GRANT_TYPE,
+	JIRA_AUTH_GRANT_TYPE_REFRESH_TOKEN,
+	JIRA_AUTH_REDIRECT_URI,
+	JIRA_AUTH_SCOPES,
+	JIRA_AUTH_STATE_PREFIX,
+	cacheInstance,
+	LIFERAY_TESTRAY_REDIRECT_AUTHORIZATION,
 });
+
+class FetcherError extends Error {
+	public info: any;
+	public status?: number;
+
+	constructor(message: string) {
+		super(message);
+	}
+}
 
 class Jira extends JiraAuth {
 	public async getIssue(ticket: string, userId: string): Promise<any> {
@@ -101,23 +112,23 @@ class Jira extends JiraAuth {
 		return jiraIssues;
 	}
 
-    public async updateIssue(ticket: string, body: unknown, userId: string) {
-        const { cloudId, token } = await this.getTokenAndCloudId(userId);
+	public async updateIssue(ticket: string, body: unknown, userId: string) {
+		const { cloudId, token } = await this.getTokenAndCloudId(userId);
 
-        const response = await fetch(
-            `${JIRA_API_BASE_URL}/ex/jira/${cloudId}/rest/api/latest/issue/${ticket}`,
-            {
-                body: JSON.stringify(body),
-                headers: {
-                    "Authorization": `Bearer ${token}`,
-                    'Content-Type': 'application/json',
-                },
-                method: 'PUT',
-            }
-        );
+		const response = await fetch(
+			`${JIRA_API_BASE_URL}/ex/jira/${cloudId}/rest/api/latest/issue/${ticket}`,
+			{
+				body: JSON.stringify(body),
+				headers: {
+					Authorization: `Bearer ${token}`,
+					"Content-Type": "application/json",
+				},
+				method: "PUT",
+			},
+		);
 
-        return response.json();
-    }
+		return response.json();
+	}
 }
 
 export default Jira;
