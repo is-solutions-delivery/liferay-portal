61f31afd861478d1a7805ffce55d02a38b606c32
Wellington Barbosa <welitonpe@gmail.com>
2023-06-15 11:26:41 -0300

    LPS-184593 Created the logic to integrate the Testray with jira
    Co-authored-by: Elvison Victor <elvison.victor@liferay.com>
    Co-authored-by: Eugenio Antunes <eugenio.antunes@liferay.com>

--- workspaces/liferay-testray-workspace/client-extensions/liferay-testray-etc-jira/src/index.ts
+++ workspaces/liferay-testray-workspace/client-extensions/liferay-testray-etc-jira/src/index.ts
@@ -12,43 +12,74 @@
  * details.
  */
 
-import { Elysia, t } from 'elysia';
+import { cors } from "@elysiajs/cors";
+import { Elysia, t } from "elysia";
 
-import Cache from './lib/Cache';
-import logger from './lib/Logger';
-import Jira from './services/jira/Jira';
-import JiraEngine from './services/jira/JiraEngine';
-import Testray from './services/liferay/Testray';
+import Cache from "./lib/Cache";
+import logger from "./lib/Logger";
+import Jira from "./services/jira/Jira";
+import JiraEngine from "./services/jira/JiraEngine";
+import Testray from "./services/liferay/Testray";
 
-const { APP_DEBUG_CACHE_ROUTER = '/cache', PORT } = Bun.env;
+const {
+	APP_DEBUG_CACHE_ROUTER = "/cache",
+	PORT,
+	LIFERAY_TESTRAY_REDIRECT_AUTHORIZATION,
+	LIFERAY_BASE_URL,
+} = Bun.env;
 
 const jira = new Jira();
 const jiraEngine = new JiraEngine();
 const testray = new Testray();
 
-const getUserId = (request: Request) =>
-    request.headers.get('liferay-user-id') ?? '20123';
+const getUserId = (request: any) => {
+	if (request?.params) {
+		return request?.params["liferay-user-id"];
+	}
+
+	return request?.headers.get("liferay-user-id");
+};
 
 const cacheInstance = Cache.getInstance();
 
 new Elysia()
-    .get('/', () => 'Testray LXC Jira Integration')
-    .get(APP_DEBUG_CACHE_ROUTER, () => {
-        logger.debug(cacheInstance);
+	.get("/", () => "Testray LXC Jira Integration")
+	.get(APP_DEBUG_CACHE_ROUTER, () => {
+		logger.debug(cacheInstance);
 
-        return cacheInstance;
-    })
-    .get('/jira/authorize', async ({ request, set }) =>
-        jira.authorize(getUserId(request), set)
-    )
-    .get('/jira/authorize/callback', async ({ query: { code, state } }) => {
-        const response = await jira.exchangeAuthorizationCode({
-            code: code as string,
-            state: state as string,
-        });
+		return cacheInstance;
+	})
+	.get("/jira/authorize/:liferay-user-id", async (request) =>
+		jira.authorize(getUserId(request), request.set),
+	)
+	.get(
+		"/jira/authorize/callback",
+		async ({ query: { code, state }, params, set }) => {
+			const response = await jira.exchangeAuthorizationCode({
+				code: code as string,
+				state: state as string,
+			});
 
-        await testray.setTestrayOAuthJiraCode(response, state as string);
+			await testray.setTestrayOAuthJiraCode(response, state as string);
 
+			return (set.redirect = `${LIFERAY_BASE_URL}${LIFERAY_TESTRAY_REDIRECT_AUTHORIZATION}`);
+		},
+	)
+	.put(
+		"/jira/ticket",
+		({ body, request }) =>
+			jiraEngine.updateIssues(body.tickets, getUserId(request)),
+		{
+			schema: {
+				body: t.Object({
+					tickets: t.Array(t.String()),
+				}),
+			},
+		},
+	)
+	.get("/jira/ticket/:ticket", ({ params: { ticket }, request }) =>
+		jira.getIssue(ticket, getUserId(request)),
+	)
 	.post(
 		"/jira/getissues",
 		({ body, request }) => jira.getIssues(body.issues, getUserId(request)),
@@ -60,3 +91,9 @@ new Elysia()
 			},
 		},
 	)
+	.use(cors())
+	.listen(Number(PORT), ({ hostname, port }) =>
+		logger.info(
+			`🦊 Testray LXC Integration with Elysia is running at ${hostname}:${port}`,
+		),
+	);
--- workspaces/liferay-testray-workspace/client-extensions/liferay-testray-etc-jira/src/services/liferay/Testray.ts
+++ workspaces/liferay-testray-workspace/client-extensions/liferay-testray-etc-jira/src/services/liferay/Testray.ts
@@ -12,81 +12,116 @@
  * details.
  */
 
-import SearchBuilder from '../../lib/SearchBuilder';
-import { getSearchParams } from '../../lib/SearchParams';
+import SearchBuilder from "../../lib/SearchBuilder";
+import { getSearchParams } from "../../lib/SearchParams";
 import {
-    APIResponse,
-    JiraAuthorizePayload,
-    TestrayIssue,
-    TestrayJiraOAuth,
-} from '../../lib/Types';
-import LiferayAuth from './LiferayAuth';
+	APIResponse,
+	JiraAuthorizePayload,
+	TestrayIssue,
+	TestrayJiraOAuth,
+} from "../../lib/Types";
+import LiferayAuth from "./LiferayAuth";
 
-const { JIRA_AUTH_STATE_PREFIX } = Bun.env;
+const { JIRA_AUTH_STATE_PREFIX, LIFERAY_AUTH_TOKEN } = Bun.env;
 
 class Testray extends LiferayAuth {
-    public async getOAuthJira(userId: string) {
-        const searchParams = getSearchParams({
-            filter: SearchBuilder.eq('r_testrayJiraOAuth_userId', userId),
-        });
+	public async getOAuthJira(userId: string) {
+		const searchParams = getSearchParams({
+			filter: SearchBuilder.eq("r_testrayJiraOAuth_userId", userId),
+		});
 
-        const response: APIResponse<TestrayJiraOAuth> = await this.fetcher(
-            `/o/c/testrayjiraoauths?${searchParams}`
-        );
+		const response: APIResponse<TestrayJiraOAuth> = await this.fetcher(
+			`/o/c/testrayjiraoauthses?${searchParams}`,
+			{
+				headers: {
+					Authorization: LIFERAY_AUTH_TOKEN as string,
+					"Content-Type": "application/json",
+				},
+			},
+		);
 
-        if (response.totalCount) {
-            return response.items[0];
-        }
+		if (response.totalCount) {
+			return response.items[0];
+		}
 
-        throw new Error(`No Jira accessToken for ${userId}`);
-    }
+		throw new Error(`No Jira accessToken for ${userId}`);
+	}
 
-    public async setTestrayOAuthJiraCode(
-        { access_token, expires_in, refresh_token }: JiraAuthorizePayload,
-        state: string
-    ) {
-        const [, userId] = state.split(JIRA_AUTH_STATE_PREFIX as string);
+	public async setTestrayOAuthJiraCode(
+		{ access_token, expires_in, refresh_token }: JiraAuthorizePayload,
+		state: string,
+	) {
+		const [, userId] = state.split(JIRA_AUTH_STATE_PREFIX as string);
 
-        try {
-            const response = await this.getOAuthJira(userId);
+		try {
+			const response = await this.getOAuthJira(userId);
 
-            await this.fetcher(`/o/c/testrayjiraoauths/${response.id}`, {
-                body: JSON.stringify({
-                    accessToken: access_token,
-                    expiresIn: expires_in,
-                    r_testrayJiraOAuth_userId: userId,
-                }),
-                method: 'PUT',
-            });
-        } catch {
-            await this.fetcher('/o/c/testrayjiraoauths', {
-                body: JSON.stringify({
-                    accessToken: access_token,
-                    expiresIn: expires_in,
-                    r_testrayJiraOAuth_userId: userId,
-                    refreshToken: refresh_token,
-                }),
-                method: 'POST',
-            });
-        }
-    }
+			await this.fetcher(`/o/c/testrayjiraoauthses/${response.id}`, {
+				body: JSON.stringify({
+					accessToken: access_token,
+					expiresIn: expires_in,
+					r_testrayJiraOAuth_userId: userId,
+				}),
+				headers: {
+					Authorization: LIFERAY_AUTH_TOKEN as string,
+					"Content-Type": "application/json",
+				},
+				method: "PUT",
+			});
 
-    public async getIssues(issues: string[]) {
-        const searchParams = getSearchParams({
-            fields: 'id,name,issueToCaseResultsIssues.r_caseResultToCaseResultsIssues_c_caseResult.r_caseToCaseResult_c_case.name,issueToCaseResultsIssues.r_caseResultToCaseResultsIssues_c_caseResult.r_caseToCaseResult_c_case.priority',
-            filter: SearchBuilder.in('name', issues),
-            nestedFields:
-                'issueToCaseResultsIssues,r_caseResultToCaseResultsIssues_c_caseResult',
-            nestedFieldsDepth: 3,
-            pageSize: 100,
-        });
+			await this.fetcher(
+				`/o/headless-admin-user/v1.0/user-accounts/${userId}`,
+				{
+					body: JSON.stringify({
+						jiraAuthorization: true,
+					}),
+					headers: {
+						Authorization: LIFERAY_AUTH_TOKEN as string,
+						"Content-Type": "application/json",
+					},
+					method: "PATCH",
+				},
+			);
+		} catch {
+			await this.fetcher("/o/c/testrayjiraoauthses", {
+				body: JSON.stringify({
+					accessToken: access_token,
+					expiresIn: expires_in,
+					r_testrayJiraOAuth_userId: userId,
+					refreshToken: refresh_token,
+				}),
+				headers: {
+					Authorization: LIFERAY_AUTH_TOKEN as string,
+					"Content-Type": "application/json",
+				},
+				method: "POST",
+			});
+		}
+	}
 
-        const response: APIResponse<TestrayIssue> = await this.fetcher(
-            decodeURIComponent(`/o/c/issues?${searchParams}`)
-        );
+	public async getIssues(issues: string[]) {
+		const searchParams = getSearchParams({
+			fields:
+				"id,name,issueToCaseResultsIssues.r_caseResultToCaseResultsIssues_c_caseResult.r_caseToCaseResult_c_case.name,issueToCaseResultsIssues.r_caseResultToCaseResultsIssues_c_caseResult.r_caseToCaseResult_c_case.priority",
+			filter: SearchBuilder.in("name", issues),
+			nestedFields:
+				"issueToCaseResultsIssues,r_caseResultToCaseResultsIssues_c_caseResult",
+			nestedFieldsDepth: 3,
+			pageSize: 100,
+		});
 
-        return response.items;
-    }
+		const response: APIResponse<TestrayIssue> = await this.fetcher(
+			decodeURIComponent(`/o/c/issues?${searchParams}`),
+			{
+				headers: {
+					Authorization: LIFERAY_AUTH_TOKEN as string,
+					"Content-Type": "application/json",
+				},
+			},
+		);
+
+		return response.items;
+	}
 }
 
 export default Testray;
