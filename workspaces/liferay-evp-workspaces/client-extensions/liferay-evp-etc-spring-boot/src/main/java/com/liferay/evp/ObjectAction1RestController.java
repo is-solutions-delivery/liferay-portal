75fcb867a6df3fa0ac8b22d8b146c0b5f9684fde
Elvinho <elvison.victor@liferay.com>
2023-08-01 10:23:54 -0300

    LPS-190132 Update Request Status When Organization Is Review

--- workspaces/liferay-evp-workspaces/client-extensions/liferay-evp-etc-spring-boot/src/main/java/com/liferay/evp/ObjectAction1RestController.java
+++ workspaces/liferay-evp-workspaces/client-extensions/liferay-evp-etc-spring-boot/src/main/java/com/liferay/evp/ObjectAction1RestController.java
@@ -1,22 +1,15 @@
 /**
- * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
- *
- * This library is free software; you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation; either version 2.1 of the License, or (at your option)
- * any later version.
- *
- * This library is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
 package com.liferay.evp;
 
-import java.util.Objects;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+
+import java.util.HashMap;
+
+import org.json.JSONObject;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,112 +19,60 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.reactive.function.client.WebClient;
-
-import org.json.JSONObject;
-import java.time.Duration;
-
-import reactor.util.retry.Retry;
-import org.springframework.http.MediaType;
 
 /**
- * @author Raymond Augé
- * @author Gregory Amerson
- * @author Brian Wing Shun Chan
+ * @author Elvison Victor
  */
 @RequestMapping("/object/action/1")
 @RestController
 public class ObjectAction1RestController extends BaseRestController {
 
-	public long evpRequestId;
-	String token;
-
 	@PostMapping
 	public ResponseEntity<String> post(
-			@AuthenticationPrincipal Jwt jwt, @RequestBody String json) {
+		@AuthenticationPrincipal Jwt jwt, @RequestBody String json) {
 
-		JSONObject request = new JSONObject(json);
+		JSONObject evpRequestJSONObject = new JSONObject(json);
 
-		JSONObject objectEntryDTOEVPRequest = request.getJSONObject("objectEntryDTOEVPRequest");
+		JSONObject objectEntryDTOEVPRequestJSONObject =
+			evpRequestJSONObject.getJSONObject("objectEntryDTOEVPRequest");
 
-		JSONObject properties = objectEntryDTOEVPRequest.getJSONObject("properties");
+		JSONObject propertiesJSONObject =
+			objectEntryDTOEVPRequestJSONObject.getJSONObject("properties");
 
-		evpRequestId = objectEntryDTOEVPRequest.getLong("id");
+		long evpOrganizationId = propertiesJSONObject.getLong(
+			"r_organization_c_evpOrganizationId");
 
-		long organizationId = properties.getLong("r_organization_c_evpOrganizationId");
-		token = jwt.getTokenValue();
-		_get(organizationId, token);
+		get(
+			response -> {
+				JSONObject evpOrganizationJSONObject = new JSONObject(response);
+
+				String evpOrganizationStatus =
+					evpOrganizationJSONObject.getJSONObject(
+						"organizationStatus"
+					).getString(
+						"key"
+					);
+
+				if (evpOrganizationStatus.equals("awaitingApprovalOnEvp")) {
+					HashMap<String, HashMap<String, String>> map =
+						HashMapBuilder.<String, HashMap<String, String>>put(
+							"requestStatus",
+							HashMapBuilder.put(
+								"key", "awaitOrganizationReview"
+							).put(
+								"name", "Awaiting Organization Review"
+							).build()
+						).build();
+
+					put(
+						new JSONObject(map), jwt,
+						"/o/c/evprequests/" +
+							objectEntryDTOEVPRequestJSONObject.getLong("id"));
+				}
+			},
+			jwt, "/o/c/evporganizations/" + evpOrganizationId);
 
 		return new ResponseEntity<>(json, HttpStatus.OK);
-
 	}
-
-	private static final Log _log = LogFactory.getLog(
-			ObjectAction1RestController.class);
-
-	private JSONObject _get(long organizationId, String token) {
-
-		return new JSONObject(
-				Objects.requireNonNull(WebClient.create(
-						_lxcDXPServerProtocol + "://" + _lxcDXPMainDomain)
-						.get()
-						.uri("/o/c/evporganizations/" + organizationId)
-						.header(
-								"Authorization", "Bearer " + token)
-						.retrieve().bodyToMono(
-								String.class)
-						.retryWhen(
-								Retry.backoff(
-										3, Duration.ofSeconds(1))
-										.doAfterRetry(retrySignal -> _log.info("Retrying request")))
-						.doOnNext(
-								output -> {
-
-									JSONObject organization = new JSONObject(output);
-
-									JSONObject organizationStatus = organization.getJSONObject("organizationStatus");
-
-									String statusKeyOrganization = organizationStatus.getString("key");
-
-									JSONObject itemsStatus = new JSONObject();
-									JSONObject requestStatus = new JSONObject();
-
-									String path = "/o/c/evprequests/" + evpRequestId;
-									if (statusKeyOrganization.equals("awaitingApprovalOnEvp")) {
-										itemsStatus
-												.put("key", "awaitOrganizationReview")
-												.put("name", "Awaiting Organization Review");
-										requestStatus.put("requestStatus", itemsStatus);
-
-										_put(requestStatus.toString(), path);
-									}
-								})
-						.subscribe()));
-	}
-
-	private void _put(String bodyValue, String path) {
-		WebClient.create(
-				_lxcDXPServerProtocol + "://" + _lxcDXPMainDomain)
-				.patch()
-				.uri(uriBuilder -> uriBuilder.path(path).build())
-				.accept(
-						MediaType.APPLICATION_JSON)
-				.contentType(
-						MediaType.APPLICATION_JSON)
-				.header(
-						"Authorization", "Bearer " + token)
-				.bodyValue(
-						bodyValue)
-				.retrieve().bodyToMono(
-						Void.class)
-				.subscribe();
-	}
-
-	@Value("${com.liferay.lxc.dxp.mainDomain}")
-	private String _lxcDXPMainDomain;
-
-	@Value("${com.liferay.lxc.dxp.server.protocol}")
-	private String _lxcDXPServerProtocol;
 
 }
