1b6d8482bdf255c69f2fdaf9d17b8c7527bd4515
Aline Pereira <aline.pereira@liferay.com>
2023-08-11 16:46:46 -0300

    WIP

--- modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/routes/customer-portal/pages/Project/TeamMembers/TeamMembers.jsx
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/routes/customer-portal/pages/Project/TeamMembers/TeamMembers.jsx
@@ -3,10 +3,13 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
+import {useQuery} from '@apollo/client';
 import {useEffect} from 'react';
 import {useOutletContext} from 'react-router-dom';
 import i18n from '../../../../../common/I18n';
 import useCurrentKoroneikiAccount from '../../../../../common/hooks/useCurrentKoroneikiAccount';
+import {getAccountSubscriptionGroups} from '../../../../../common/services/liferay/graphql/queries';
+import TeamMemberFooter from '../../../components/IncidentContactCard/TeamMemberFooter';
 import ManageProductUsers from './components/ManageProductUsers/ManageProductUsers';
 import TeamMembersTable from './components/TeamMembersTable/TeamMembersTable';
 
@@ -14,6 +17,22 @@ const TeamMembers = () => {
 	const {setHasQuickLinksPanel, setHasSideMenu} = useOutletContext();
 	const {data, loading} = useCurrentKoroneikiAccount();
 	const koroneikiAccount = data?.koroneikiAccountByExternalReferenceCode;
+
+	const {data: dataSubscriptionGroups} = useQuery(
+		getAccountSubscriptionGroups,
+		{
+			variables: {
+				filter: `accountKey eq '${koroneikiAccount?.accountKey}' and hasActivation eq true`,
+			},
+		}
+	);
+
+	const accountSubscriptionGroups =
+		dataSubscriptionGroups?.c.accountSubscriptionGroups?.items;
+
+	const accountSubscriptionGroupsNames = accountSubscriptionGroups?.map(
+		(group) => group.name
+	);
 
 	useEffect(() => {
 		setHasQuickLinksPanel(false);
@@ -40,6 +59,20 @@ const TeamMembers = () => {
 					koroneikiAccount={koroneikiAccount}
 					loading={loading}
 				/>
+
+				{(accountSubscriptionGroupsNames?.includes('Analytics Cloud') ||
+					accountSubscriptionGroupsNames?.includes(
+						'Liferay Experience Cloud'
+					) ||
+					accountSubscriptionGroupsNames?.includes('LXC - SM')) && (
+					<TeamMemberFooter
+						accountSubscriptionGroupsNames={
+							accountSubscriptionGroupsNames
+						}
+						koroneikiAccount={koroneikiAccount}
+						loading={loading}
+					/>
+				)}
 			</div>
 		</>
 	);
