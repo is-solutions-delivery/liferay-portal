cde5b4078edbf46c0dda265087c5466f65b57b71
Nilton Vieira <nilton.vieira@liferay.com>
2023-08-29 16:17:34 -0300

    LPS-194712 addOrUpdateSiteNavigationMenuItem

--- modules/apps/site-navigation/site-navigation-service/src/main/java/com/liferay/site/navigation/service/impl/SiteNavigationMenuItemLocalServiceImpl.java
+++ modules/apps/site-navigation/site-navigation-service/src/main/java/com/liferay/site/navigation/service/impl/SiteNavigationMenuItemLocalServiceImpl.java
@@ -52,6 +52,41 @@ public class SiteNavigationMenuItemLocalServiceImpl
 	extends SiteNavigationMenuItemLocalServiceBaseImpl {
 
 	@Override
+	public SiteNavigationMenuItem addOrUpdateSiteNavigationMenuItem(
+			String externalReferenceCode, long userId, long groupId,
+			long siteNavigationMenuId, long parentSiteNavigationMenuItemId,
+			String type, int order, String typeSettings,
+			ServiceContext serviceContext)
+		throws Exception {
+
+		SiteNavigationMenuItem siteNavigationMenuItem =
+			siteNavigationMenuItemPersistence.fetchByERC_G(
+				externalReferenceCode, groupId);
+
+		if (siteNavigationMenuItem != null) {
+			siteNavigationMenuItem = addSiteNavigationMenuItem(
+				userId, groupId, siteNavigationMenuId,
+				parentSiteNavigationMenuItemId, type, order, typeSettings,
+				serviceContext);
+
+			siteNavigationMenuItem.setExternalReferenceCode(
+				externalReferenceCode);
+
+			siteNavigationMenuItem = siteNavigationMenuItemPersistence.update(
+				siteNavigationMenuItem);
+		}
+		else {
+			siteNavigationMenuItem = updateSiteNavigationMenuItem(
+				userId,
+				siteNavigationMenuItem.getParentSiteNavigationMenuItemId(),
+				groupId, siteNavigationMenuId, parentSiteNavigationMenuItemId,
+				type, order, typeSettings);
+		}
+
+		return siteNavigationMenuItem;
+	}
+
+	@Override
 	public SiteNavigationMenuItem addSiteNavigationMenuItem(
 			long userId, long groupId, long siteNavigationMenuId,
 			long parentSiteNavigationMenuItemId, String type, int order,
