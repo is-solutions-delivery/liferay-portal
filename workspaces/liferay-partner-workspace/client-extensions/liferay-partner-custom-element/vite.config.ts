e1c24bdd8ca5f5aed67ef03b9e2b9f5b6ed57c12
thaynam <lazaro@thaynam.com>
2023-09-18 15:15:38 -0300

    LPS-196572 Create partner custom element client extention

---
+++ workspaces/liferay-partner-workspace/client-extensions/liferay-partner-custom-element/vite.config.ts
@@ -0,0 +1,39 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import react from '@vitejs/plugin-react';
+import {defineConfig} from 'vite';
+
+// https://vitejs.dev/config/
+
+export default defineConfig({
+	build: {
+		outDir: 'build/vite',
+		rollupOptions: {
+			output: {
+				assetFileNames: 'assets/[name][extname]',
+				chunkFileNames: '[name]-[hash].js',
+				entryFileNames: '[name]-[hash].js',
+			},
+		},
+	},
+	experimental: {
+		renderBuiltUrl(filename: string) {
+			if (
+				filename.endsWith('.css') ||
+				filename.endsWith('.png') ||
+				filename.endsWith('.svg')
+			) {
+				return `/o/liferay-partner-custom-element/${filename}`;
+			}
+
+			return filename;
+		},
+	},
+	esbuild: {
+		logOverride: {'this-is-undefined-in-esm': 'silent'},
+	},
+	plugins: [react()],
+});
