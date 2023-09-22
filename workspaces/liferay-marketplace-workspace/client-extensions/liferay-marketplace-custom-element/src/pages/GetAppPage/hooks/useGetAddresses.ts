d9c013d8f527f5144ec2a0e4d01a096b6656476f
Clarice Brasil <clarice.brasil@liferay.com>
2023-09-22 14:38:35 -0300

    LPS-197054 Add hooks to improve getApp Page

---
+++ workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/pages/GetAppPage/hooks/useGetAddresses.ts
@@ -0,0 +1,31 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import { useEffect, useState } from "react";
+
+import { getAccountAddressesFromCommerce } from "../../../utils/api";
+
+const useGetAddresses = (selectedAccount: Account | undefined) => {
+    const [addresses, setAddresses] = useState<BillingAddress[]>([]);
+
+    useEffect(() => {
+        const getAddresses = async () => {
+            if (selectedAccount?.id) {
+              const billingAddresses = await getAccountAddressesFromCommerce(
+                selectedAccount?.id as number
+              );
+      
+              setAddresses(billingAddresses.items);
+            }
+          };
+          getAddresses();
+    }, [selectedAccount?.id]);
+
+    return {
+        addresses
+    }
+};
+
+export default useGetAddresses;
