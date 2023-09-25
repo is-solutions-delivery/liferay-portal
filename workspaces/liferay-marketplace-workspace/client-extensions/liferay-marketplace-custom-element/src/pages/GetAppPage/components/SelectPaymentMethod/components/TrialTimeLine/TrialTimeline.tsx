fc3ab9f894f9187e77605b6a824b16a29f021797
Clarice Brasil <clarice.brasil@liferay.com>
2023-09-25 15:39:49 -0300

    LPS-196550 Create directory structure

--- workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/pages/GetAppPage/components/SelectPaymentMethod/components/TrialTimeLine/TrialTimeline.tsx
+++ workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/pages/GetAppPage/components/SelectPaymentMethod/components/TrialTimeLine/TrialTimeline.tsx
@@ -5,9 +5,9 @@
 
 /* eslint-disable react/no-unescaped-entities */
 
-import circleFill from '../../../assets/icons/circle_fill_icon.svg';
-import radioSelected from '../../../assets/icons/radio_button_checked_2_icon.svg';
-import timeline from '../../../assets/images/timeline.png';
+import circleFill from '../../../../../../assets/icons/circle_fill_icon.svg';
+import radioSelected from '../../../../../../assets/icons/radio_button_checked_2_icon.svg';
+import timeline from '../../../../../../assets/images/timeline.png';
 
 import './TrialTimeline.scss';
 
