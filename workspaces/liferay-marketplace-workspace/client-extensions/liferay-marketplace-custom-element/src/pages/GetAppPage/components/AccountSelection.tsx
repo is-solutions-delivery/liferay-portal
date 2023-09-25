8681f80ed7e216bc04b31684a7cedda8a4671b45
Clarice Brasil <clarice.brasil@liferay.com>
2023-09-25 17:50:51 -0300

    LPS-196550 Disable dependency from useEffect

--- workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/pages/GetAppPage/components/AccountSelection.tsx
+++ workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/pages/GetAppPage/components/AccountSelection.tsx
@@ -59,6 +59,7 @@ const AccountSelection = ({onSelectAccount, setUserAccount, userAccount}: Accoun
 
 	useEffect(() => {
 		getUserAccountList();
+	// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
 
 	return (
