e6ee4230d4284998490be03cf0a1d480c7a27525
Elias Santos <elias.santos@liferay.com>
2023-09-26 21:09:37 -0300

    LPS-197378 Adjustment of clarity solution page labels

--- modules/apps/site-initializer/site-initializer-liferay-marketplace/src/main/resources/site-initializer/fragments/group/marketplace-base-fragments/fragments/app-banner/index.js
+++ modules/apps/site-initializer/site-initializer-liferay-marketplace/src/main/resources/site-initializer/fragments/group/marketplace-base-fragments/fragments/app-banner/index.js
@@ -1,7 +1,35 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-expressions */
+/* eslint-disable @liferay/portal/no-global-fetch */
 /**
  * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
+
+const baseURL = Liferay.ThemeDisplay.getPortalURL();
+
+const fetcher = async (url, {method = 'GET', ...options} = {}) => {
+	const response = await fetch(`${baseURL}${url}`, {
+		headers: {
+			'content-type': 'application/json',
+			'x-csrf-token': Liferay.authToken,
+		},
+		method,
+		...options,
+	});
+
+	if (response.ok) {
+		if (method === 'DELETE' || response.status === 204) {
+			return;
+		}
+
+		return response.json();
+	}
+
+	console.error('Failed to fetch user data:', response.status);
+
+	throw new Error(response.json());
+};
 
 Liferay.on('copy-link', () => {
 	const url = window.location.href;
@@ -13,7 +41,6 @@ Liferay.on('copy-link', () => {
 });
 
 Liferay.on('contact-publisher', () => {
-	// eslint-disable-next-line no-undef
 	const emailAddress = fragmentElement.querySelector(
 		'.banner__contact-button'
 	).value;
@@ -23,7 +50,6 @@ Liferay.on('contact-publisher', () => {
 });
 
 Liferay.on('start-trial', () => {
-	// eslint-disable-next-line no-undef
 	const finalURL = `purchase-product-form?productId=${configuration.productId}`;
 	window.location.href = `${Liferay.ThemeDisplay.getPortalURL()}${getSiteURL()}/${finalURL}`;
 });
@@ -37,3 +63,45 @@ const getSiteURL = () => {
 
 	return '';
 };
+
+const getProducts = async () => {
+	const products = fetcher(
+		`/o/headless-commerce-admin-catalog/v1.0/products`
+	);
+
+	return products;
+};
+
+const getUrlString = (url) => {
+	const urlSplit = url.split('/');
+	const filteredUrl = urlSplit[urlSplit.length - 1];
+
+	return filteredUrl;
+};
+
+const setLabels = async () => {
+	const products = (await getProducts()).items;
+
+	const currentUrl = Liferay.currentURL;
+	const labelDiv = document.getElementById('categoryLabel');
+
+	const filteredLabels = products
+		.filter((product) => product.urls.en_US === getUrlString(currentUrl))
+		.map(
+			(product) =>
+				product.categories
+					.filter(
+						(category) =>
+							category.vocabulary === 'marketplace app category'
+					)
+					.map(
+						(category) =>
+							`<span class="banner__product-tag rounded py-2 px-3 mr-2">${category.name}</span>`
+					)
+					.join('') // Use join para unir as strings das categorias
+		);
+
+	labelDiv.innerHTML = filteredLabels;
+};
+
+setLabels();
