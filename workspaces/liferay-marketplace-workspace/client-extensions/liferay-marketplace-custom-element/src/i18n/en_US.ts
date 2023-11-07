a3c7557b42aa143c478b8225014cfa614bf7a26a
Joana Grego <joana.lima@liferay.com>
2023-11-07 09:47:14 -0300

    ISSD-1509 Create License Details Modal

--- workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/i18n/en_US.ts
+++ workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/i18n/en_US.ts
@@ -5,17 +5,30 @@
 
 export default {
 	'access-console': 'Access Console',
+	'activation-status': 'Activation Status',
 	'back-to-my-apps': 'Back to My Apps',
 	'create-license-key': 'Create License Key',
 	'create-new-licenses-and-they-will-show-up-here':
 		'Create new licenses and they will show up here.',
+	'deactivate': 'Deactivate',
 	'details': 'Details',
 	'download-app': 'Download App',
+	'download-key': 'Download Key',
+	'environment': 'Environment',
+	'environment-type': 'Environment Type',
+	'expiration-date': 'Expiration Date',
+	'host-name': 'Host Name',
+	'instance-size': 'Instance Size',
+	'ip-addresses': 'IP Addresses',
+	'key-type': 'Key Type',
 	'licenses': 'Licenses',
+	'mac-addresses': 'Mac Addresses',
 	'manage': 'Manage',
 	'no-licenses-yet': 'No Licenses yet',
 	'no-results-found': 'No results found',
 	'sorry-there-are-no-results-found': 'Sorry, there are no results found',
+	'start-date': 'Start Date',
+	'status': 'Status',
 	'the-order-must-be-completed-before-licensing-this-app.':
 		'The order must be completed before licensing this app.',
 	'this-order-must-be-completed-before-downloading-this-app.':
