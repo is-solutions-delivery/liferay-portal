121d1c520e2d1bf03dbe2fd72dce57e3c2429d55
thaynam <lazaro@thaynam.com>
2023-11-14 16:42:22 -0300

    ISSD-1620 Add get opportunity by erc

--- workspaces/liferay-partner-workspace/client-extensions/liferay-partner-custom-element/src/common/utils/constants/filters.ts
+++ workspaces/liferay-partner-workspace/client-extensions/liferay-partner-custom-element/src/common/utils/constants/filters.ts
@@ -3,7 +3,6 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import {ResourceName} from '../../services/liferay/object/enum/resourceName';
 import {getCamelCase} from '../getCamelCase';
 
 const todayDate = new Date();
