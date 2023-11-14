9501f4c140cdb6304c0a377e085e0d0631b28db5
Clarice Brasil <clarice.brasil@liferay.com>
2023-11-14 10:47:34 -0300

    ISSD-1680 Create Offering Type CheckboxCard

---
+++ workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/pages/ProvideAppBuildPage/components/OfferingTypeCheckbox.tsx
@@ -0,0 +1,37 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import {CheckboxCard} from '../../../components/CheckboxCard/CheckboxCard';
+
+interface OfferingType {
+	description: string;
+	label: string;
+}
+
+interface OfferingTypeCheckboxProps {
+	handleSelectCheckbox: (label: string) => void;
+	offeringTypes: OfferingType[];
+	selectedValue: string[];
+}
+
+export default function OfferingTypeCheckbox({
+	handleSelectCheckbox,
+	offeringTypes,
+	selectedValue,
+}: OfferingTypeCheckboxProps) {
+	return (
+		<div className="provide-app-build-page-app-build-checkbox-container">
+			{offeringTypes?.map((type) => (
+				<CheckboxCard
+					checked={selectedValue.includes(type?.label)}
+					description={type.description}
+					key={type.label}
+					label={type.label}
+					onChange={handleSelectCheckbox}
+				/>
+			))}
+		</div>
+	);
+}
