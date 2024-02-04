5a66ba9afb1c5c5b5cbd3099cd97bcda8fa508e5
Keven Leone <keven.leone@liferay.com>
2024-02-04 11:31:15 -0300

    LPD-16394 Add Validations for Free DXP Apps

--- workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/pages/GetApp/pages/Account.tsx
+++ workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/pages/GetApp/pages/Account.tsx
@@ -3,9 +3,12 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
+import {useOutletContext} from 'react-router-dom';
+
 import {useMarketplaceContext} from '../../../context/MarketplaceContext';
 import i18n from '../../../i18n';
 import {useGetAppContext} from '../GetAppContextProvider';
+import {GetAppOutletContext} from '../GetAppOutlet';
 import AccountSelection from '../components/AccountSelection';
 import Container from '../containers/Container';
 import getProductPriceModel from '../utils/getProductPriceModel';
@@ -15,22 +18,34 @@ const GetAppPage = () => {
 		{
 			account,
 			formState: {isValid},
+			isCloudApp,
 			product,
 			stepState,
 		},
 		dispatch,
 	] = useGetAppContext();
+	const {handleGetApp, loading} = useOutletContext<GetAppOutletContext>();
 	const {isFreeApp} = getProductPriceModel(product);
 	const {myUserAccount} = useMarketplaceContext();
+
+	const isFreeDXPApp = isFreeApp && !isCloudApp;
 
 	return (
 		<Container
 			className="d-flex flex-column"
 			footerProps={{
 				primaryButtonProps: {
-					children: i18n.translate('continue'),
-					disabled: !isValid,
-					onClick: stepState.onNext,
+					children: i18n.translate(
+						isFreeDXPApp ? 'get-app' : 'continue'
+					),
+					disabled: !isValid || loading,
+					onClick: () => {
+						if (isFreeDXPApp) {
+							return handleGetApp();
+						}
+
+						stepState.onNext();
+					},
 				},
 				secondaryButtonProps: {visible: false},
 			}}
