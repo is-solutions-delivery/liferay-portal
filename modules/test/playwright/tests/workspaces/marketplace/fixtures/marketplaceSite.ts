9319557fc89343fa335894e0504f8cfc0857e086
Keven Leone <keven.leone@liferay.com>
2024-03-27 08:41:11 -0300

    LPD-21474 Add Logo

--- modules/test/playwright/tests/workspaces/marketplace/fixtures/marketplaceSite.ts
+++ modules/test/playwright/tests/workspaces/marketplace/fixtures/marketplaceSite.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 /**
  * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
@@ -15,23 +16,27 @@ export interface Marketplace {
 }
 
 const SITE_EXTERNAL_REFERENCE_CODE = 'marketplace-site-initializer';
+const TEMPLATE_KEY = 'com.liferay.site.initializer.liferay.marketplace';
 const SITE_NAME = 'Marketplace';
 
 export const marketplaceSiteFixture = test.extend<Marketplace>({
 	marketplace: [
 		async ({apiHelpers, page}, use) => {
-			let site = await apiHelpers.headlessSite.getSiteByExternalReferenceCode(
+			let site = await apiHelpers.headlessSite.getSiteByERC(
 				SITE_EXTERNAL_REFERENCE_CODE
 			);
 
-			if (site.status === 'NOT_FOUND') {
+			console.log(1, 'Site', site);
+
+			if ((site as any).status === 'NOT_FOUND') {
 				site = await apiHelpers.headlessSite.createSite(SITE_NAME, {
 					externalReferenceCode: SITE_EXTERNAL_REFERENCE_CODE,
-					templateKey:
-						'com.liferay.site.initializer.liferay.marketplace',
+					templateKey: TEMPLATE_KEY,
 					templateType: 'site-initializer',
 				});
 			}
+
+			console.log(2, 'Site', site);
 
 			expect(site.name).toBe(SITE_NAME);
 			expect(site.id).toBeGreaterThan(0);
