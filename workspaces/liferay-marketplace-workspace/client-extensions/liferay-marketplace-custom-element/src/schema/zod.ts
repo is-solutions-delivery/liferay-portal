814878068c2fa95a3770d6cd42beb47864e258a6
Wellington Barbosa <welitonpe@gmail.com>
2024-09-19 11:51:59 -0300

    LPD-35999 Created a hook to get provisioning data, add transtaltions and zod validation schema

--- workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/schema/zod.ts
+++ workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/schema/zod.ts
@@ -130,6 +130,17 @@ const zodSchema = {
 			})
 			.optional(),
 	}),
+	installProductSchema: z.object({
+		environment: z.string(),
+		orderItemId: z.number(),
+		project: z.object({
+			availabilityToProduct: z.boolean(),
+			environments: z.array(z.string()),
+			rootProjectId: z.string(),
+			rootProjectPlanUsage: z.any(),
+		}),
+		step: z.string(),
+	}),
 	invitedNewMember: z.object({
 		emailAddress: z
 			.string()
