dc2fd71dab88c4aa9b36761b00204266038ae400
Keven Leone <keven.leone@liferay.com>
2025-01-08 17:43:03 -0300

    LPD-37874 Export Marketplace components
    Co-authored-by: Wellington Barbosa <wellington.barbosa@liferay.com>

---
+++ modules/apps/marketplace/marketplace-web/src/main/resources/META-INF/resources/js/index.ts
@@ -0,0 +1,13 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+export * from './components/ConnectionWithMarketplaceNeededModal';
+export * from './components/MarketplaceProducts';
+export * from './components/MarketplaceStorefront';
+
+export * from './hooks/useMarketplaceAuthorization';
+export * from './hooks/useMarketplaceProducts';
+
+export * from './types';
