4890cc18a56615561dc7d8cf5511433bee249130
Lucas Emanuel <lemmg@cin.ufpe.br>
2025-01-29 13:45:52 -0300

    LRSD-8054 sf

--- workspaces/liferay-learn-workspace/client-extensions/liferay-learn-site-initializer/site-initializer/fragments/group/liferay-learn/fragments/unassigned-top-banner/index.js
+++ workspaces/liferay-learn-workspace/client-extensions/liferay-learn-site-initializer/site-initializer/fragments/group/liferay-learn/fragments/unassigned-top-banner/index.js
@@ -4,6 +4,7 @@
  */
 if (!themeDisplay.isSignedIn()) {
 	document.querySelector('.learn-banner').style.marginTop = '150px';
+
 	document.addEventListener('DOMContentLoaded', () => {
 		document.querySelector('.icon-x').addEventListener('click', () => {
 			document.querySelector('.banner-sign-in').style.display = 'none';
