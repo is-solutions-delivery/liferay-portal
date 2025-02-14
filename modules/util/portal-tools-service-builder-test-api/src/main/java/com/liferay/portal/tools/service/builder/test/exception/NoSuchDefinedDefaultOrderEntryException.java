d823db730e93aef2e0507b3d4d74c14a7dae1009
Tina Tian <tina.tian@liferay.com>
2025-02-14 14:11:41 -0800

    LPD-48950 Auto SF

--- modules/util/portal-tools-service-builder-test-api/src/main/java/com/liferay/portal/tools/service/builder/test/exception/NoSuchDefinedDefaultOrderEntryException.java
+++ modules/util/portal-tools-service-builder-test-api/src/main/java/com/liferay/portal/tools/service/builder/test/exception/NoSuchDefinedDefaultOrderEntryException.java
@@ -2,6 +2,7 @@
  * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
+
 package com.liferay.portal.tools.service.builder.test.exception;
 
 import com.liferay.portal.kernel.exception.NoSuchModelException;
@@ -9,7 +10,8 @@ import com.liferay.portal.kernel.exception.NoSuchModelException;
 /**
  * @author Brian Wing Shun Chan
  */
-public class NoSuchDefinedDefaultOrderEntryException extends NoSuchModelException {
+public class NoSuchDefinedDefaultOrderEntryException
+	extends NoSuchModelException {
 
 	public NoSuchDefinedDefaultOrderEntryException() {
 	}
@@ -18,7 +20,9 @@ public class NoSuchDefinedDefaultOrderEntryException extends NoSuchModelExceptio
 		super(msg);
 	}
 
-	public NoSuchDefinedDefaultOrderEntryException(String msg, Throwable throwable) {
+	public NoSuchDefinedDefaultOrderEntryException(
+		String msg, Throwable throwable) {
+
 		super(msg, throwable);
 	}
 
--- modules/util/portal-tools-service-builder-test-api/src/main/java/com/liferay/portal/tools/service/builder/test/exception/NoSuchUndefinedDefaultOrderEntryException.java
+++ modules/util/portal-tools-service-builder-test-api/src/main/java/com/liferay/portal/tools/service/builder/test/exception/NoSuchUndefinedDefaultOrderEntryException.java
@@ -2,6 +2,7 @@
  * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
+
 package com.liferay.portal.tools.service.builder.test.exception;
 
 import com.liferay.portal.kernel.exception.NoSuchModelException;
@@ -9,7 +10,8 @@ import com.liferay.portal.kernel.exception.NoSuchModelException;
 /**
  * @author Brian Wing Shun Chan
  */
-public class NoSuchUndefinedDefaultOrderEntryException extends NoSuchModelException {
+public class NoSuchUndefinedDefaultOrderEntryException
+	extends NoSuchModelException {
 
 	public NoSuchUndefinedDefaultOrderEntryException() {
 	}
@@ -18,7 +20,9 @@ public class NoSuchUndefinedDefaultOrderEntryException extends NoSuchModelExcept
 		super(msg);
 	}
 
-	public NoSuchUndefinedDefaultOrderEntryException(String msg, Throwable throwable) {
+	public NoSuchUndefinedDefaultOrderEntryException(
+		String msg, Throwable throwable) {
+
 		super(msg, throwable);
 	}
 
