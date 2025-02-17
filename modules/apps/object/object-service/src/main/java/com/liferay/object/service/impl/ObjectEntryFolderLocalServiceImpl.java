dd9e5af5b590816911c55027f8490cce4a02daab
Mikel Lorza <mikel.lorza@liferay.com>
2025-02-17 13:44:42 +0100

    LPD-48769 Add indexable annotation to the methods that does not have it by default

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryFolderLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryFolderLocalServiceImpl.java
@@ -20,6 +20,8 @@ import com.liferay.portal.kernel.dao.orm.RestrictionsFactoryUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.ResourceConstants;
 import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.search.Indexable;
+import com.liferay.portal.kernel.search.IndexableType;
 import com.liferay.portal.kernel.service.ResourceLocalService;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.UserLocalService;
@@ -44,6 +46,7 @@ import org.osgi.service.component.annotations.Reference;
 public class ObjectEntryFolderLocalServiceImpl
 	extends ObjectEntryFolderLocalServiceBaseImpl {
 
+	@Indexable(type = IndexableType.REINDEX)
 	@Override
 	public ObjectEntryFolder addObjectEntryFolder(
 			String externalReferenceCode, long userId, long groupId,
@@ -174,6 +177,7 @@ public class ObjectEntryFolderLocalServiceImpl
 		return objectEntryFolder;
 	}
 
+	@Indexable(type = IndexableType.REINDEX)
 	@Override
 	public ObjectEntryFolder updateObjectEntryFolder(
 			long userId, long objectEntryFolderId,
