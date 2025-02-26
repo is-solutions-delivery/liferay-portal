e0c4b71466997abb1c9fca02724d8d4e1e00e439
Ana Beatriz Ximenes Alves <anabeatriz.alves@liferay.com>
2025-02-26 19:02:43 -0300

    LRSD-7727 - SF

--- workspaces/liferay-dotcom-workspace/client-extensions/liferay-dotcom-etc-spring-boot/src/main/java/com/liferay/dotcom/ObjectActionEventRegistrationStatusUpdateRestController.java
+++ workspaces/liferay-dotcom-workspace/client-extensions/liferay-dotcom-etc-spring-boot/src/main/java/com/liferay/dotcom/ObjectActionEventRegistrationStatusUpdateRestController.java
@@ -1,5 +1,5 @@
 /**
- * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
@@ -11,10 +11,8 @@ import com.liferay.petra.string.StringBundler;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-import org.json.JSONArray;
 import org.json.JSONObject;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -30,7 +28,8 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RequestMapping("/object/action/event-registration-status-update")
 @RestController
-public class ObjectActionEventRegistrationStatusUpdateRestController extends BaseRestController {
+public class ObjectActionEventRegistrationStatusUpdateRestController
+	extends BaseRestController {
 
 	@PostMapping
 	public ResponseEntity<String> post(
@@ -43,14 +42,14 @@ public class ObjectActionEventRegistrationStatusUpdateRestController extends Bas
 					"/o/c/eventregistrations/", _getEventRegistrationId(json),
 					"?fields=r_eventRegistration_c_eventId")));
 
-		long eventID = eventRegistrationResponseJSONObject.getLong("r_eventRegistration_c_eventId");
+		long eventID = eventRegistrationResponseJSONObject.getLong(
+			"r_eventRegistration_c_eventId");
 
 		JSONObject eventResponseJSONObject = new JSONObject(
 			get(
 				"Bearer " + jwt.getTokenValue(),
 				StringBundler.concat(
-					"/o/c/events/", eventID,
-					"?fields=eventCapacityStatus")));
+					"/o/c/events/", eventID, "?fields=eventCapacityStatus")));
 
 		patch(
 			"Bearer " + jwt.getTokenValue(),
@@ -60,13 +59,16 @@ public class ObjectActionEventRegistrationStatusUpdateRestController extends Bas
 			"/o/c/eventregistrations/" + _getEventRegistrationId(json));
 
 		if (_log.isInfoEnabled()) {
-			_log.info("Updated Event Registration Status " + _getEventRegistrationId(json));
+			_log.info(
+				"Updated Event Registration Status " +
+					_getEventRegistrationId(json));
 		}
 
 		return new ResponseEntity<>(json, HttpStatus.OK);
 	}
 
-	//	revisar
+	// 	revisar
+
 	private long _getEventRegistrationId(String json) {
 		JSONObject jsonObject = new JSONObject(json);
 
@@ -84,16 +86,20 @@ public class ObjectActionEventRegistrationStatusUpdateRestController extends Bas
 	}
 
 	private JSONObject _getPayloadJSONObject(JSONObject eventStatusJSONObject) {
-		JSONObject eventResgistrationStatus = new JSONObject(
-		).put("key", eventStatusJSONObject.getString("key")
-		).put("name", eventStatusJSONObject.getString("name"));
+		JSONObject eventResgistrationStatusJSONObject = new JSONObject(
+		).put(
+			"key", eventStatusJSONObject.getString("key")
+		).put(
+			"name", eventStatusJSONObject.getString("name")
+		);
 
 		return new JSONObject(
 		).put(
-			"eventRegistrationStatus", eventResgistrationStatus
+			"eventRegistrationStatus", eventResgistrationStatusJSONObject
 		);
 	}
 
 	private static final Log _log = LogFactory.getLog(
 		ObjectActionEventRegistrationStatusUpdateRestController.class);
+
 }
