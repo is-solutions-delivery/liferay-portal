3f04e87534ca13ce7ecc0fb74bd4847b2bb38abe
Lucas Emanuel <lemmg@cin.ufpe.br>
2025-03-11 09:45:38 -0300

    LRSD-8105 Make the admonitions on LMS match the design standard

--- workspaces/liferay-learn-workspace/client-extensions/liferay-learn-site-initializer/site-initializer/fragments/group/lms/fragments/lms-lesson-display-page-layout/index.js
+++ workspaces/liferay-learn-workspace/client-extensions/liferay-learn-site-initializer/site-initializer/fragments/group/lms/fragments/lms-lesson-display-page-layout/index.js
@@ -3,19 +3,47 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
+const admonitionsElements = document.querySelectorAll(
+	'.portlet-msg-alert, .portlet-msg-error, .portlet-msg-info'
+);
+
 const copyToClipboardButtons = document.querySelectorAll(
 	'.copy-to-clipboard-button'
 );
+
+admonitionsElements.forEach((admonitionElement) => {
+	if (admonitionElement) {
+		let brFound = false;
+		const textAdmonitionNodes = [];
+
+		for (let i = 0; i < admonitionElement.childNodes.length; i++) {
+			const admonitionChildNode = admonitionElement.childNodes[i];
+
+			if (admonitionChildNode.nodeName === "BR") {
+				brFound = true;
+			} else if (brFound && admonitionChildNode.nodeType === Node.TEXT_NODE && admonitionChildNode.nodeValue.trim() !== "") {
+				textAdmonitionNodes.push(admonitionChildNode);
+			}
+		}
+
+		if (textAdmonitionNodes.length) {
+			const textAdmonitionDiv = document.createElement("div");
+			textAdmonitionDiv.classList.add("text-admonition");
+			textAdmonitionNodes.forEach(node => textAdmonitionDiv.appendChild(node));
+			admonitionElement.appendChild(textAdmonitionDiv);
+		}
+	}
+});
 
 copyToClipboardButtons.forEach((button) => {
 	button.addEventListener('click', () => {
 		const codeToolbar = button.closest('.code-toolbar');
 
 		if (codeToolbar) {
-			if (codeToolbar.querySelector('code.language-bash').innerText) {
+			if (codeToolbar.querySelector('code.language-css').innerText) {
 				navigator.clipboard
 					.writeText(
-						codeToolbar.querySelector('code.language-bash')
+						codeToolbar.querySelector('code.language-css')
 							.innerText
 					)
 					.then(() => {
