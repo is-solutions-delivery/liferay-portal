12fbd51149f94ae4c6e4a90e4dbb8cc1ae0bc629
Caleb Hall <caleb.hall@liferay.com>
2025-05-20 09:56:57 -0300

    LPD-55357 Create and add ManagementToolbar to ListView

---
+++ workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/components/ListView/components/ManagementToolbar.tsx
@@ -0,0 +1,147 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import Button, {ClayButtonWithIcon} from '@clayui/button';
+import {ClayDropDownWithItems} from '@clayui/drop-down';
+import {Item} from '@clayui/drop-down/lib/Items';
+import {ClayInput} from '@clayui/form';
+import Icon from '@clayui/icon';
+import {ClayResultsBar} from '@clayui/management-toolbar';
+import ManagementToolbar from '@clayui/management-toolbar/lib/ManagementToolbar';
+import {useContext, useState} from 'react';
+
+import {ListViewContext, ListViewTypes} from '../hooks/ListViewContext';
+import i18n from '../../../i18n';
+
+export type ManagementToolbarProps = {
+	filterItems?: Item[];
+	results?: number;
+};
+
+export function ListViewManagementToolbar({
+	filterItems,
+	results,
+}: ManagementToolbarProps) {
+	const [open, setOpen] = useState(false);
+	const [{filters, keywords}, dispatch] = useContext(ListViewContext);
+	const [searchInput, setSearchInput] = useState(keywords);
+
+	const [filterKey] = Object.keys(filters.filter);
+
+	const filter = filters.filter[filterKey];
+
+	const clearSearch = () => {
+		dispatch({
+			payload: null,
+			type: ListViewTypes.SET_CLEAR,
+		});
+		setSearchInput('');
+	};
+
+	return (
+		<ManagementToolbar>
+			{filterItems?.length && (
+				<ClayDropDownWithItems
+					items={filterItems.map((item) => {
+						return {
+							...item,
+							onClick: () => item.onClick?.(dispatch as any),
+						};
+					})}
+					onActiveChange={setOpen}
+					trigger={
+						<Button className="nav-link" displayType="unstyled">
+							<span className="mr-3">
+								<Icon symbol="filter" />
+							</span>
+							<span className="navbar-text-truncate">Filter</span>
+						</Button>
+					}
+					triggerIcon={open ? 'caret-bottom' : 'caret-top'}
+				/>
+			)}
+			<ManagementToolbar.Search
+				onSubmit={(event) => {
+					event.preventDefault();
+
+					dispatch({
+						payload: searchInput,
+						type: ListViewTypes.SET_SEARCH,
+					});
+				}}
+			>
+				<ClayInput.Group>
+					<ClayInput.GroupItem>
+						<ClayInput
+							aria-label="Search"
+							className="form-control input-group-inset input-group-inset-after"
+							onChange={(event) =>
+								setSearchInput(event.target.value)
+							}
+							placeholder="Search"
+							type="text"
+							value={searchInput}
+						/>
+						<ClayInput.GroupInsetItem after tag="span">
+							<ClayButtonWithIcon
+								aria-label="Search"
+								displayType="unstyled"
+								onClick={() => {
+									dispatch({
+										payload: searchInput,
+										type: ListViewTypes.SET_SEARCH,
+									});
+								}}
+								symbol="search"
+							/>
+							{keywords || filter && (
+								<ClayButtonWithIcon
+									aria-label="Clear"
+									displayType="unstyled"
+									onClick={() => {
+										clearSearch();
+									}}
+									symbol="times"
+								/>
+							)}
+						</ClayInput.GroupInsetItem>
+					</ClayInput.GroupItem>
+				</ClayInput.Group>
+			</ManagementToolbar.Search>
+			{(keywords || filter) && (
+				<div className="d-block w-100">
+					<ClayResultsBar>
+						<ClayResultsBar.Item>
+							<span className="component-text text-truncate-inline">
+								<span className="text-truncate">
+									{results} {i18n.translate('results-for')} 
+									<strong className="m-1">
+										{keywords || filter}
+									</strong>
+
+									{keywords && filter && (
+										<span>
+											{i18n.translate('and')} <strong>{filter}</strong>
+										</span>
+									)}
+								</span>
+							</span>
+						</ClayResultsBar.Item>
+
+						<ClayResultsBar.Item className="ml-auto">
+							<Button
+								className="component-link tbar-link"
+								displayType="unstyled"
+								onClick={clearSearch}
+							>
+								{i18n.translate('clear')}
+							</Button>
+						</ClayResultsBar.Item>
+					</ClayResultsBar>
+				</div>
+			)}
+		</ManagementToolbar>
+	);
+}
