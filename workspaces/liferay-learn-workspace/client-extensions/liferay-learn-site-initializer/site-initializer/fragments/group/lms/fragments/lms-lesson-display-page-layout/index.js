9e30b976fe037f31952b5518f133de7b91a615ea
Lucas Emanuel <lemmg@cin.ufpe.br>
2025-05-22 16:35:31 -0300

    LRSD-9526 Implement the front-end to add Text to Speech feature to Learn text content

--- workspaces/liferay-learn-workspace/client-extensions/liferay-learn-site-initializer/site-initializer/fragments/group/lms/fragments/lms-lesson-display-page-layout/index.js
+++ workspaces/liferay-learn-workspace/client-extensions/liferay-learn-site-initializer/site-initializer/fragments/group/lms/fragments/lms-lesson-display-page-layout/index.js
@@ -1,11 +1,17 @@
 /**
- * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
 const copyToClipboardButtons = document.querySelectorAll(
 	'.copy-to-clipboard-button'
 );
+
+const textToSpeechContainer = document.querySelector(
+	'.text-to-speech-container'
+);
+
+const componentHtmlH1 = document.querySelector('.component-html h1');
 
 copyToClipboardButtons.forEach((button) => {
 	button.addEventListener('click', () => {
@@ -32,3 +38,9 @@ copyToClipboardButtons.forEach((button) => {
 		}
 	});
 });
+
+if (componentHtmlH1 && componentHtmlH1.parentElement) {
+	componentHtmlH1.insertAdjacentElement('afterend', textToSpeechContainer);
+} else {
+	console.error("Elemento h1 ou seu pai com a classe 'component-html' não encontrado.");
+}
