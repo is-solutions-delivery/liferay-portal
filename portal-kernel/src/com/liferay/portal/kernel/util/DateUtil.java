3c01fdcf167f6ec618cfe44eaef472b8412246cf
Brian Chan <brian.chan@liferay.com>
2025-07-13 19:45:48 +0200

    LPD-60293 SF

--- portal-kernel/src/com/liferay/portal/kernel/util/DateUtil.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/DateUtil.java
@@ -179,9 +179,9 @@ public class DateUtil {
 		Calendar calendar = Calendar.getInstance();
 
 		calendar.set(Calendar.HOUR_OF_DAY, 0);
+		calendar.set(Calendar.MILLISECOND, 0);
 		calendar.set(Calendar.MINUTE, 0);
 		calendar.set(Calendar.SECOND, 0);
-		calendar.set(Calendar.MILLISECOND, 0);
 
 		calendar.add(Calendar.DATE, 1);
 
