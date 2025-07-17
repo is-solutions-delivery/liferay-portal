8ef23fb237b9379525602d95228aecd1c5a86858
Ricardo Mariz <ricardo.mariz@liferay.com>
2025-07-17 14:34:06 -0300

    LPD-59154 SF

--- workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/pages/SSADashboard/components/TrialListView/TrialListView.tsx
+++ workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/pages/SSADashboard/components/TrialListView/TrialListView.tsx
@@ -1,14 +1,20 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
 import {format} from 'date-fns';
+
 import ListView, {ListViewProps} from '../../../../components/ListView';
 import {ManagementToolbarProps} from '../../../../components/ListView/components/ManagementToolbar';
-import i18n from '../../../../i18n';
-import TrialStatus from '../TrialStatus/TrialStatus';
-import ExtensionStatus from '../ExtensionStatus/ExtensionStatus';
-import {getExtensionStatusFromTrialSettings} from '../../util';
 import {OrderCustomFields} from '../../../../enums/Order';
+import i18n from '../../../../i18n';
 import {Action} from '../../../../utils/constants';
 import {TrialSettings} from '../../enums/SSATrials';
 import {SSASettings} from '../../types';
+import {getExtensionStatusFromTrialSettings} from '../../util';
+import ExtensionStatus from '../ExtensionStatus/ExtensionStatus';
+import TrialStatus from '../TrialStatus/TrialStatus';
 
 type TrialsListViewProps = {
 	actions: Action[];
