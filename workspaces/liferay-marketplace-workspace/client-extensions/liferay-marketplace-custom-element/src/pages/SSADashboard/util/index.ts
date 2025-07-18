06384765225a219a3afa29b6b21fca03cc3a069a
Ricardo Mariz <ricardo.mariz@liferay.com>
2025-07-18 14:40:45 -0300

    LPD-59154 Add filters

--- workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/pages/SSADashboard/util/index.ts
+++ workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/pages/SSADashboard/util/index.ts
@@ -3,8 +3,6 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import SearchBuilder from '../../../core/SearchBuilder';
-import {OrderTypes} from '../../../enums/Order';
 import {ExtendRequestStatus} from '../enums/SSATrials';
 
 export function getExtensionStatusFromTrialSettings(trialSettings: string) {
@@ -22,16 +20,10 @@ export default function getSSATrialsResourceURL(
 	channelId: number,
 	accountId: string
 ) {
-	const url = `/o/headless-commerce-delivery-order/v1.0/channels/${channelId}/accounts/${accountId}/placed-orders?${new URLSearchParams(
+	return `/o/headless-commerce-delivery-order/v1.0/channels/${channelId}/accounts/${accountId}/placed-orders?${new URLSearchParams(
 		{
-			filter: SearchBuilder.eq(
-				'orderTypeExternalReferenceCode',
-				OrderTypes.SSA_SAAS
-			),
 			nestedFields: 'placedOrderItems',
 			sort: 'createDate:desc',
 		}
 	)}`;
-
-	return url;
 }
