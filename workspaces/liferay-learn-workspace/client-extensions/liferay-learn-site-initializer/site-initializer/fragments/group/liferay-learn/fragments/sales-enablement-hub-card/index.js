d2284df1caf97d052caa9f3060534aea7351b4eb
Lucas Emanuel <lemmg@cin.ufpe.br>
2025-07-21 09:19:47 -0300

    LRSD-10432 sf

--- workspaces/liferay-learn-workspace/client-extensions/liferay-learn-site-initializer/site-initializer/fragments/group/liferay-learn/fragments/sales-enablement-hub-card/index.js
+++ workspaces/liferay-learn-workspace/client-extensions/liferay-learn-site-initializer/site-initializer/fragments/group/liferay-learn/fragments/sales-enablement-hub-card/index.js
@@ -7,61 +7,67 @@ const getElementByClass = (className) =>
 	document.querySelector(`.${className}`);
 
 const domElements = {
-	containerHubCard: getElementByClass('container-enablement-hub-card'),
-	cardLink: getElementByClass('sales-enablement-hub-card-link'),
-	card: getElementByClass('sales-page-card'),
-	cardDescription: getElementByClass('sales-page-card-description'),
-	cardGoToText: getElementByClass('sales-page-card-go-to-text'),
-	cardTitle: getElementByClass('sales-page-card-title'),
+	containerEnablementHubCard: getElementByClass('container-enablement-hub-card'),
+	salesEnablementHubCardLink: getElementByClass('sales-enablement-hub-card-link'),
+	salesPageCard: getElementByClass('sales-page-card'),
+	salesPageCardDescription: getElementByClass('sales-page-card-description'),
+	salesPageCardGoToText: getElementByClass('sales-page-card-go-to-text'),
+	salesPageCardTitle: getElementByClass('sales-page-card-title'),
 };
 
 const fetchUserAccounts = async () => {
 	const response = await Liferay.Util.fetch(
 		`/o/headless-admin-user/v1.0/user-accounts/${Liferay.ThemeDisplay.getUserId()}`
 	);
+
 	return response.json();
 };
 
-const updateCardUI = (isTrainer) => {
-	const {
-		card,
-		cardDescription,
-		cardGoToText,
-		cardLink,
-		cardTitle
-	} = domElements;
+const renderCardByRole = (isTrainerLoungeUserAccountRole) => {
+	const {salesEnablementHubCardLink, salesPageCard, salesPageCardDescription, salesPageCardGoToText, salesPageCardTitle} =
+		domElements;
 
-	card.classList.toggle('sales-enablement-hub-card-icon', isTrainer);
-	card.classList.toggle('sales-resources-card-icon', !isTrainer);
+		salesPageCard.classList.toggle('sales-enablement-hub-card-icon', isTrainerLoungeUserAccountRole);
+		salesPageCard.classList.toggle('sales-resources-card-icon', !isTrainerLoungeUserAccountRole);
 
-	if (isTrainer) {
-		cardTitle.textContent = 'Enablement Hub';
-		cardDescription.textContent =
+	if (isTrainerLoungeUserAccountRole) {
+		salesEnablementHubCardLink.href = '/web/sales-enablement/home';
+		salesPageCardDescription.textContent =
 			'Access exclusive assets, tools, and materials designed to help our partners and staff succeed in their roles, from sales to certified training.';
-		cardGoToText.textContent = 'Go to the Enablement Hub';
-		cardLink.href = '/web/sales-enablement/home';
-	} else {
-		cardTitle.textContent = 'Sales Resources';
-		cardDescription.textContent =
+		salesPageCardGoToText.textContent = 'Go to the Enablement Hub';
+		salesPageCardTitle.textContent = 'Enablement Hub';
+	}
+	else {
+		salesEnablementHubCardLink.href = '/web/sales-enablement/sales-resources';
+		salesPageCardDescription.textContent =
 			'Find the latest presentation decks, battle cards, reports, and other essential resources to effectively position and sell Liferay solutions.';
-		cardGoToText.textContent = 'Go to the Sales Enablement Hub';
-		cardLink.href = '/web/sales-enablement/sales-resources';
+		salesPageCardGoToText.textContent = 'Go to the Sales Enablement Hub';
+		salesPageCardTitle.textContent = 'Sales Resources';
+		
 	}
 };
 
 document.addEventListener('DOMContentLoaded', async () => {
-	const user = await fetchUserAccounts();
+	const userAccount = await fetchUserAccounts();
 
-	const userExternalReferenceCode = user.roleBriefs.map((userRole) => userRole.externalReferenceCode);
-	const userGroupNames = user.userGroupBriefs.map((userGroup) => userGroup.name);
-	
+	const userExternalReferenceCode = userAccount.roleBriefs.map(
+		(userRole) => userRole.externalReferenceCode
+	);
+	const userGroupNames = userAccount.userGroupBriefs.map(
+		(userGroup) => userGroup.name
+	);
+
 	const isEmployeeOrPartner = (userGroupNames) =>
-	userGroupNames.includes('Employees') || userGroupNames.includes('Partners');
+		userGroupNames.includes('Employees') ||
+		userGroupNames.includes('Partners');
 
 	if (isEmployeeOrPartner(userGroupNames)) {
-		domElements.containerHubCard.classList.remove('hide');
+		domElements.containerEnablementHubCard.classList.remove('hide');
 
-		updateCardUI(userExternalReferenceCode.includes('TRAINERS-LOUNGE-CONTENT-ADMIN') ||
-		userExternalReferenceCode.includes('TRAINERS-LOUNGE-USER'));
+		renderCardByRole(
+			userExternalReferenceCode.includes(
+				'TRAINERS-LOUNGE-CONTENT-ADMIN'
+			) || userExternalReferenceCode.includes('TRAINERS-LOUNGE-USER')
+		);
 	}
 });
