f88b5796e62d900adb86a8e7365082007699e59f
Keven Leone <keven.santos.sz@gmail.com>
2025-10-31 13:45:07 -0300

    LPD-66707 SF

--- modules/integrations/vercel/commerce/src/app/data.ts
+++ modules/integrations/vercel/commerce/src/app/data.ts
@@ -3,10 +3,9 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import {SearchBuilder} from '../utils/search-builder';
-
 import {WithLiferay} from '../liferay/index';
 import {PageProduct} from '../types/product';
+import {SearchBuilder} from '../utils/search-builder';
 
 export async function getProductsPage({
 	keywords,
