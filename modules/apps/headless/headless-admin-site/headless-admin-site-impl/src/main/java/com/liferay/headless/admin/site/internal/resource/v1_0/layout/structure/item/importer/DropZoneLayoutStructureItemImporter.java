5b6a60b4876d3c42d607c7f879775b33b610b571
Mikel Lorza <mikel.lorza@liferay.com>
2025-11-05 17:42:53 +0100

    LPD-70446 Set dropzone internal configuration

--- modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/resource/v1_0/layout/structure/item/importer/DropZoneLayoutStructureItemImporter.java
+++ modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/resource/v1_0/layout/structure/item/importer/DropZoneLayoutStructureItemImporter.java
@@ -5,12 +5,20 @@
 
 package com.liferay.headless.admin.site.internal.resource.v1_0.layout.structure.item.importer;
 
+import com.liferay.headless.admin.site.dto.v1_0.DropZonePageElementDefinition;
+import com.liferay.headless.admin.site.dto.v1_0.FragmentReference;
 import com.liferay.headless.admin.site.dto.v1_0.PageElement;
+import com.liferay.headless.admin.site.internal.dto.v1_0.util.FragmentEntryReferenceUtil;
 import com.liferay.headless.admin.site.internal.resource.v1_0.layout.structure.item.importer.context.LayoutStructureItemImporterContext;
 import com.liferay.headless.admin.site.internal.resource.v1_0.util.LayoutStructureUtil;
 import com.liferay.layout.util.structure.DropZoneLayoutStructureItem;
 import com.liferay.layout.util.structure.LayoutStructure;
 import com.liferay.layout.util.structure.LayoutStructureItem;
+import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.Validator;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Eudaldo Alonso
@@ -46,7 +54,59 @@ public class DropZoneLayoutStructureItemImporter
 						pageElement.getPosition());
 		}
 
+		DropZonePageElementDefinition dropZonePageElementDefinition =
+			(DropZonePageElementDefinition)
+				pageElement.getPageElementDefinition();
+
+		if (dropZonePageElementDefinition == null) {
+			return dropZoneLayoutStructureItem;
+		}
+
+		dropZoneLayoutStructureItem.setAllowNewFragmentEntries(
+			dropZoneLayoutStructureItem.isAllowNewFragmentEntries());
+		dropZoneLayoutStructureItem.setFragmentEntryKeys(
+			_getFragmentEntryKeys(
+				dropZonePageElementDefinition,
+				layoutStructureItemImporterContext));
+
 		return dropZoneLayoutStructureItem;
+	}
+
+	private List<String> _getFragmentEntryKeys(
+			DropZonePageElementDefinition dropZonePageElementDefinition,
+			LayoutStructureItemImporterContext
+				layoutStructureItemImporterContext)
+		throws Exception {
+
+		if (ArrayUtil.isEmpty(
+				dropZonePageElementDefinition.getAllowedFragments())) {
+
+			return null;
+		}
+
+		List<String> fragmentKeys = new ArrayList<>();
+
+		for (FragmentReference fragmentReference :
+				dropZonePageElementDefinition.getAllowedFragments()) {
+
+			FragmentEntryReferenceUtil.FragmentEntryReference
+				fragmentEntryReference =
+					FragmentEntryReferenceUtil.getFragmentEntryReference(
+						layoutStructureItemImporterContext.getCompanyId(),
+						fragmentReference,
+						layoutStructureItemImporterContext.getGroupId());
+
+			if (Validator.isNotNull(fragmentEntryReference.getRendererKey())) {
+				fragmentKeys.add(fragmentEntryReference.getRendererKey());
+			}
+			else if (Validator.isNotNull(
+						fragmentEntryReference.getFragmentEntryKey())) {
+
+				fragmentKeys.add(fragmentEntryReference.getFragmentEntryKey());
+			}
+		}
+
+		return fragmentKeys;
 	}
 
 }
