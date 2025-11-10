ba1694bb4c3c1fb7798580b4ad490764ce8ffd65
Caleb Hall <caleb.hall@liferay.com>
2025-11-10 15:47:34 -0300

    LPD-70541 Added manage user and manage roles modals

---
+++ workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/pages/SSADashboard/modals/InviteUserModal.tsx
@@ -0,0 +1,192 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import {useForm} from 'react-hook-form';
+import {Size} from '@clayui/modal/lib/types';
+import ClayLoadingIndicator from '@clayui/loading-indicator';
+
+import {Input} from '../../../components/Input/Input';
+import zodSchema, {z, zodResolver} from '../../../schema/zod';
+import MultiSelect from '../../../components/MultiSelect/MultiSelect';
+import {useState} from 'react';
+import i18n from '../../../i18n';
+import {getFilteredItems} from '../utils';
+import {Label} from '../../../components/MarketplaceForm/Label';
+import HeadlessAdminUser from '../../../services/rest/HeadlessAdminUser';
+import {useMarketplaceContext} from '../../../context/MarketplaceContext';
+import {Liferay} from '../../../liferay/liferay';
+import marketplaceOAuth2 from '../../../services/oauth/Marketplace';
+import Modal from '../../../components/Modal';
+import Button from '@clayui/button';
+import {UserRoleTypes} from '../../../enums/Account';
+import {ssaRoles} from '../constants';
+
+export type Item = {
+	key: string;
+	value: string;
+	label: string;
+};
+
+type InviteUserModalPops = {
+	mutate: any;
+	modal: {
+		observer: any;
+		onClose: () => void;
+		open: boolean;
+	};
+};
+
+type ModalForm = {
+	emailAddress: string;
+	roles: Item[];
+};
+
+const InviteUserModal = ({modal, mutate}: InviteUserModalPops) => {
+	const {properties} = useMarketplaceContext();
+	const {
+		formState: {errors, isSubmitting},
+		setValue,
+		register,
+		handleSubmit,
+	} = useForm<ModalForm>({
+		resolver: zodResolver(zodSchema.ssaInviteUsers),
+	});
+
+	const [selectedItems, setSelectedItems] = useState<Item[]>([]);
+
+	const onSubmit = async (formData: ModalForm) => {
+		const [accountRoles, user, userRoles] = await Promise.all([
+			HeadlessAdminUser.getAccountRoles(
+				properties.accountExternalReferenceCode
+			),
+			HeadlessAdminUser.postAccountUserAccountByEmailAddress(
+				properties.accountExternalReferenceCode,
+				formData.emailAddress
+			),
+			HeadlessAdminUser.getRolesPage(
+				new URLSearchParams({pageSize: '-1'})
+			),
+		]);
+
+		const ssaUser = userRoles.items.find(
+			(userRole) => userRole.name === UserRoleTypes.SSA_USER
+		);
+
+		const roles = formData.roles.map((role) =>
+			accountRoles.items.find(
+				(accountRole) => accountRole.name === role.value
+			)
+		);
+
+		try {
+			await Promise.all([
+				marketplaceOAuth2.postAssignRoleUserAccount(
+					ssaUser?.id as number,
+					user.id
+				),
+				roles.map((role) =>
+					HeadlessAdminUser.sendRoleAccountUser(
+						Number(Liferay.CommerceContext.account?.accountId),
+						role?.id as number,
+						user.id
+					)
+				),
+			]);
+		}
+		catch {
+			return Liferay.Util.openToast({
+				message: i18n.translate('unable-to-assign-roles'),
+				title: i18n.translate('error'),
+				type: 'danger',
+			});
+		}
+
+		Liferay.Util.openToast({
+			message: i18n.translate('your-request-completed-successfully'),
+			title: i18n.translate('success'),
+			type: 'success',
+		});
+
+		mutate({revalidate: true});
+
+		return modal.onClose();
+	};
+
+	return (
+		<Modal
+			observer={modal.observer}
+			size={'md' as Size}
+			title={i18n.translate('add-new-trial')}
+			visible={modal.open}
+		>
+			<form id="invite-form" onSubmit={handleSubmit(onSubmit)}>
+				<p>
+					{i18n.translate(
+						'use-their-email-address-to-invite-them-as-an-ssa-user-or-admin-and-define-their-access-level'
+					)}
+				</p>
+
+				<Label>{i18n.translate('email')}</Label>
+
+				<Input
+					{...register('emailAddress')}
+					required
+					placeholder={i18n.translate('email')}
+					errorMessage={errors.emailAddress?.message}
+				/>
+
+				<Label>{i18n.translate('roles')}</Label>
+
+				<MultiSelect
+					disabledClearAll
+					errorMessage={errors.roles?.message}
+					inputName={i18n.translate('roles')}
+					multiselectKey={`area-${
+						getFilteredItems(selectedItems, ssaRoles).length
+					}`}
+					onItemsChange={(roles: Item[]) => {
+						const filteredRoles = roles.filter((role) =>
+							ssaRoles.some((ssaRole) => ssaRole.key === role.key)
+						);
+
+						setSelectedItems(filteredRoles);
+						setValue('roles', roles);
+					}}
+					required
+					selectedItems={selectedItems}
+					sourceItems={getFilteredItems(selectedItems, ssaRoles)}
+				/>
+			</form>
+
+			<hr />
+
+			<div className="d-flex justify-content-end">
+				<Button
+					className="mr-2"
+					disabled={isSubmitting}
+					displayType="secondary"
+					onClick={modal.onClose}
+				>
+					{i18n.translate('cancel')}
+				</Button>
+
+				<Button
+					disabled={isSubmitting}
+					displayType="primary"
+					onClick={handleSubmit(onSubmit)}
+				>
+					<div className="align-items-center d-flex">
+						{isSubmitting && (
+							<ClayLoadingIndicator className="mr-3 my-0" />
+						)}
+						{i18n.translate('invite')}
+					</div>
+				</Button>
+			</div>
+		</Modal>
+	);
+};
+
+export default InviteUserModal;
---
+++ workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/pages/SSADashboard/modals/ManageUserRolesModal.tsx
@@ -0,0 +1,164 @@
+import MultiSelect from '../../../components/MultiSelect/MultiSelect';
+import {useForm} from 'react-hook-form';
+import {z} from '../../../schema/zod';
+import {zodResolver} from '@hookform/resolvers/zod';
+import {ssaRoles} from '../constants';
+import {getFilteredItems} from '../utils';
+import {Liferay} from '../../../liferay/liferay';
+import HeadlessAdminUser from '../../../services/rest/HeadlessAdminUser';
+import i18n from '../../../i18n';
+import {KeyedMutator} from 'swr';
+
+const formSchema = z.object({
+	roles: z
+		.array(z.object({value: z.string()}))
+		.nonempty(i18n.translate('at-least-one-role-must-be-provided')),
+});
+
+type FormValues = z.infer<typeof formSchema>;
+
+type ManageUserModalProps = {
+	accountERC: string;
+	mutate: KeyedMutator<any>;
+	onClose: () => void;
+	user: UserAccount;
+};
+
+const ManageUserModal = ({
+	accountERC,
+	mutate,
+	onClose,
+	user,
+}: ManageUserModalProps) => {
+	const ssaAccount = user.accountBriefs.find(
+		(account) => account.externalReferenceCode === accountERC
+	);
+
+	const currentRoles =
+		ssaAccount?.roleBriefs
+			.filter((role) =>
+				ssaRoles.some((ssaRole) => ssaRole.key === role.name)
+			)
+			.map((role) => ({
+				key: role.name,
+				value: role.name,
+				label: role.name,
+			})) || [];
+
+	const {formState, setValue, handleSubmit, watch} = useForm<FormValues>({
+		defaultValues: {roles: currentRoles},
+		resolver: zodResolver(formSchema),
+	});
+
+	const selectedRoles = watch('roles');
+
+	const onSubmit = async (formData: FormValues) => {
+		try {
+			const {items: roles} =
+				await HeadlessAdminUser.getAccountRoles(accountERC);
+
+			const intialSet = new Set(currentRoles.map((role) => role.value));
+			const newSet = new Set(formData.roles.map((role) => role.value));
+
+			const rolesToAdd = roles.filter(
+				(role) => !intialSet.has(role.name) && newSet.has(role.name)
+			);
+
+			const rolesToRemove = roles.filter(
+				(role) => intialSet.has(role.name) && !newSet.has(role.name)
+			);
+
+			await Promise.all([
+				...rolesToRemove.map((role) =>
+					HeadlessAdminUser.deleteRoleAccountUser(
+						Number(Liferay.CommerceContext.account?.accountId),
+						role.id,
+						user.id
+					)
+				),
+				...rolesToAdd.map((role) =>
+					HeadlessAdminUser.sendRoleAccountUser(
+						Number(Liferay.CommerceContext.account?.accountId),
+						role.id,
+						user.id
+					)
+				),
+			]);
+
+			const updatedRoleBriefs = roles.filter((role) =>
+				newSet.has(role.name)
+			);
+
+			mutate(
+				(users?: APIResponse<UserAccount>) => {
+					return {
+						...users,
+						items: users?.items.map((prevUser) => {
+							if (prevUser.id !== user.id) {
+								return prevUser;
+							}
+
+							return {
+								...prevUser,
+								accountBriefs: prevUser.accountBriefs.map(
+									(account) => {
+										if (
+											account.externalReferenceCode !==
+											accountERC
+										) {
+											return account;
+										}
+
+										return {
+											...account,
+											roleBriefs: updatedRoleBriefs,
+										};
+									}
+								),
+							};
+						}),
+					};
+				},
+				{revalidate: false}
+			);
+		}
+		catch {
+			Liferay.Util.openToast({
+				message: i18n.translate('unable-to-assign-roles'),
+				title: i18n.translate('error'),
+				type: 'danger',
+			});
+		}
+
+		Liferay.Util.openToast({
+			message: i18n.translate('user-roles-successfully-updated'),
+			title: i18n.translate('success'),
+		});
+
+		onClose();
+	};
+
+	return (
+		<form id="manage-roles" onSubmit={handleSubmit(onSubmit)}>
+			<p>
+				{i18n.translate(
+					'manage-the-roles-associated-with-this-user-roles-determine-what-features-permissions-and-areas-of-the-platform-the-user-can-access-so-updating-them-allows-you-to-control-their-level-of-access-and-responsibilities'
+				)}
+			</p>
+
+			<MultiSelect
+				disabledClearAll
+				errorMessage={formState.errors.roles?.message}
+				inputName={i18n.translate('roles')}
+				multiselectKey={`roles-${selectedRoles.length}`}
+				onItemsChange={(roles) => {
+					setValue('roles', roles);
+				}}
+				selectedItems={selectedRoles}
+				sourceItems={getFilteredItems(selectedRoles, ssaRoles)}
+			/>
+		</form>
+	);
+};
+
+export default ManageUserModal;
