e27a0783ecbd5a2f1973ac9e7d71c7f9e2ccb736
Nilton Vieira <nilton.vieira@liferay.com>
2025-12-15 10:37:24 -0300

    LRSD-10733 reorg folders

--- workspaces/liferay-learn-workspace/client-extensions/liferay-learn-etc-spring-boot/src/main/java/com/liferay/learn/controller/LearnRestController.java
+++ workspaces/liferay-learn-workspace/client-extensions/liferay-learn-etc-spring-boot/src/main/java/com/liferay/learn/controller/LearnRestController.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.learn;
+package com.liferay.learn.controller;
 
 import com.google.auth.oauth2.GoogleCredentials;
 
