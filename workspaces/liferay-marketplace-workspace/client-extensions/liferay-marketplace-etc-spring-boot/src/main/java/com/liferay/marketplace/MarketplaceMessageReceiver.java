810d689e88b0229df44471040b8be0e5ac2b51f2
Caleb Hall <caleb.hall@liferay.com>
2026-01-27 16:30:52 -0300

    LPD-75712 Create Marketplace message receiver

---
+++ workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-etc-spring-boot/src/main/java/com/liferay/marketplace/MarketplaceMessageReceiver.java
@@ -0,0 +1,72 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.marketplace;
+
+import com.google.cloud.pubsub.v1.AckReplyConsumer;
+import com.google.cloud.pubsub.v1.MessageReceiver;
+import com.google.protobuf.ByteString;
+import com.google.pubsub.v1.PubsubMessage;
+
+import com.liferay.marketplace.constants.MarketplaceConstants;
+import com.liferay.portal.kernel.messaging.Message;
+
+import java.util.HashMap;
+import java.util.Objects;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * @author Caleb Hall
+ */
+public class MarketplaceMessageReceiver implements MessageReceiver {
+
+	public MarketplaceMessageReceiver(
+		String topic) {
+		_topic = topic;
+	}
+
+	@Override
+	public void receiveMessage(
+		PubsubMessage pubsubMessage, AckReplyConsumer ackReplyConsumer) {
+
+		ByteString byteString = pubsubMessage.getData();
+
+		String messageBody = byteString.toStringUtf8();
+
+		if (_log.isDebugEnabled()) {
+			_log.debug("Received message: " + messageBody);
+		}
+
+		try {
+			Message message = new Message();
+
+			message.setPayload(messageBody);
+			message.setValues(
+				new HashMap<String, Object>(pubsubMessage.getAttributesMap()));
+			message.setDestinationName(_topic);
+
+
+			if (Objects.equals(_topic, MarketplaceConstants.KORONEIKI_ACCOUNT_CREATE)) {
+				// PLACEHOLDER
+			} else if (Objects.equals(_topic, MarketplaceConstants.KORONEIKI_ACCOUNT_UPDATE)) {
+				// PLACEHOLDER
+			} else if (Objects.equals(_topic, MarketplaceConstants.KORONEIKI_ENTITLEMENT_CREATE)) {
+				// PLACEHOLDER
+			}
+
+			ackReplyConsumer.ack();
+		}
+		catch (Exception exception) {
+			_log.error("Error processing message", exception);
+			ackReplyConsumer.nack();
+		}
+	}
+	private static final Log _log = LogFactory.getLog(
+		MarketplaceMessageReceiver.class);
+	private final String _topic;
+
+}
