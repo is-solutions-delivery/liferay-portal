44cb645bbeb7fcf730623992e8ae2d2ea47b69a2
Keven Leone <keven.santos.sz@gmail.com>
2026-01-29 21:52:51 -0300

    LPD-77509 Add clone-template script to download vercel templates
    Co-authored-by: Renato Rego <renato.rego@liferay.com>

---
+++ modules/integrations/vercel/clone-template.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+function main {
+	TEMPLATE="${1}"
+
+	DESTINATION=$(pwd)
+
+	if [ -n "${2}" ]
+	then
+		DESTINATION="${2}"
+	fi
+
+	TEMP_DIR=$(mktemp -d)
+
+	_clone_latest_commit
+
+	_extract_template
+
+	_make_destination_a_git_repo
+}
+
+function _clone_latest_commit {
+	git clone \
+		--branch=master \
+		--depth 1 \
+		--single-branch \
+		https://github.com/liferay/liferay-portal.git "${TEMP_DIR}"
+}
+
+function _extract_template {
+	local template_origin="${TEMP_DIR}/modules/integrations/vercel/${TEMPLATE}"
+
+	echo "Moving ${template_origin} to ${DESTINATION}"
+
+	if [ ! -d "${DESTINATION}" ]
+	then
+		mkdir -p "${DESTINATION}"
+	fi
+
+	mv -v "${template_origin}" "${DESTINATION}"
+}
+
+function _make_destination_a_git_repo {
+	cd "${DESTINATION}/${TEMPLATE}" && git init && git add . && git commit --message "chore: clone TEMPLATE"
+}
+
+main "${@}"
