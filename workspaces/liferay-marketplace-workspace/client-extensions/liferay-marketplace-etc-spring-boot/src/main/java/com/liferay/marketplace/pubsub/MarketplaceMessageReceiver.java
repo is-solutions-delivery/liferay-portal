5f34233d89a0db08d6d2857dc8138e87a78f85da
Caleb Hall <caleb.hall@liferay.com>
2026-02-09 14:43:36 -0300

    LPD-75718 add check to ensure it is a marketplace product

--- workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-etc-spring-boot/src/main/java/com/liferay/marketplace/pubsub/MarketplaceMessageReceiver.java
+++ workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-etc-spring-boot/src/main/java/com/liferay/marketplace/pubsub/MarketplaceMessageReceiver.java
@@ -46,6 +46,8 @@ import org.apache.commons.logging.LogFactory;
 
 import org.json.JSONObject;
 
+import org.springframework.beans.factory.annotation.Value;
+
 /**
  * @author Caleb Hall
  */
@@ -335,6 +337,10 @@ public class MarketplaceMessageReceiver implements MessageReceiver {
 
 		String name = entitlement.getName();
 
+		if (!_productKeys.contains(name)) {
+			return;
+		}
+
 		try {
 			com.liferay.osb.koroneiki.phloem.rest.client.pagination.Page
 				<ProductPurchase> productPurchasePage =
@@ -414,6 +420,9 @@ public class MarketplaceMessageReceiver implements MessageReceiver {
 	private final Channel _channel;
 	private final KoroneikiService _koroneikiService;
 	private final MarketplaceService _marketplaceService;
+
+	@Value("${liferay.marketplace.product.keys}")
+	private String _productKeys;
 
 	private ProductPurchase _productPurchase;
 	
