20890e0a40fe34e8dbf46c7698b652990e4c5228
Beatriz Costa <beatriz.costa@liferay.com>
2026-03-03 18:20:34 -0300

    LPD-78250 Add lexicon icon fragment

--- workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-site-initializer/site-initializer/fragments/group/marketplace-base-fragments/fragments/image-container/index.js
+++ workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-site-initializer/site-initializer/fragments/group/marketplace-base-fragments/fragments/image-container/index.js
@@ -1,5 +1,13 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
 
-const productIcon = document.getElementById(`product-icon-${configuration.fragmentId}`);
-const productIconImg = document.querySelector(`.product-icon-img-${configuration.fragmentId}`);
+const productIcon = document.getElementById(
+	`product-icon-${configuration.fragmentId}`
+);
+const productIconImg = document.querySelector(
+	`.product-icon-img-${configuration.fragmentId}`
+);
 
 productIconImg.src = productIcon.textContent;
