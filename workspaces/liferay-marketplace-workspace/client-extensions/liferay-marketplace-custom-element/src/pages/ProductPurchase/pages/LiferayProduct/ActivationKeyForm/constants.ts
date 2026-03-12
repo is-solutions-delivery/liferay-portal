e00b4ce49fd672693dfccb0f63e8368e1d837ca1
Keven Leone <keven.santos.sz@gmail.com>
2026-03-12 10:57:42 -0300

    LPD-79520 Add ActivationKey Form for Free DXP Activation Keys
    Co-authored-by: Wellington Barbosa <wellington.barbosa@liferay.com>

---
+++ workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/pages/ProductPurchase/pages/LiferayProduct/ActivationKeyForm/constants.ts
@@ -0,0 +1,31 @@
+export const PURPOSE_OPTIONS = [
+	{
+		subtitle: 'For students or individuals upskilling.',
+		title: 'Personal Learning / Education',
+		value: 'personal-learning-education',
+	},
+	{
+		subtitle: 'Testing for a specific project at work..',
+		title: 'Proof of Concept (POC)',
+		value: 'proof-of-concept',
+	},
+	{
+		subtitle: 'Developers building or debugging integrations.',
+		title: 'Development & Testing',
+		value: 'development-and-testing',
+	},
+	{
+		subtitle: 'For non-production hobbyist tools within a company.',
+		title: 'Internal Side Project',
+		value: 'internal-side-project',
+	},
+	{
+		subtitle: 'For professional firms or small companies.',
+		title: 'Small Business Production Use',
+		value: 'small-business-production',
+	},
+	{
+		title: 'Other (Please Specify)',
+		value: 'other',
+	},
+];
